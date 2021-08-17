de76ad28345db0c24abe04b817af311440082ad8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-17 22:30:25 +0200

    Enable planner

---
+++ Public/Set-O365Planner.ps1
@@ -0,0 +1,16 @@
+﻿function Set-O365Planner {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [bool] $AllowCalendarSharing
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/services/apps/planner"
+
+    $Body = @{
+        allowCalendarSharing = $AllowCalendarSharing
+        id                   = "1"
+        isPlannerAllowed     = $true
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers.Headers -Method POST -Body $Body
+    $Output
+}
