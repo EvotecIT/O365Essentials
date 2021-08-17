466506a2f7c9dc2df8f85e05e01dcfe1fb4c78b8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-17 22:30:04 +0200

    Set azure speech

---
+++ Public/Set-O365AzureSpeechServices.ps1
@@ -0,0 +1,14 @@
+﻿function Set-O365AzureSpeechServices {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [bool] $AllowTheOrganizationWideLanguageModel
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/services/apps/azurespeechservices"
+
+    $Body = @{
+        isTenantEnabled = $AllowTheOrganizationWideLanguageModel
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers.Headers -Method POST -Body $Body
+    $Output
+}
