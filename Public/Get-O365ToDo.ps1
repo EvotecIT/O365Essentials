f077aaae3b7eab272adfa1d8093f148fb4611873
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-17 13:12:34 +0200

    Get cmdlet

---
+++ Public/Get-O365ToDo.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365ToDo {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/services/apps/todo"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers.Headers
+    $Output
+}
