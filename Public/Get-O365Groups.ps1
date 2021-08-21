14a6e0200cd034f39048f78394941802dffdf4e3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-21 15:59:20 +0200

    Improvemens to Group cmdlet

--- Public/Get-O365Groups.ps1
+++ Public/Get-O365Groups.ps1
@@ -3,28 +3,18 @@
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
     )
-    $Uri = "https://admin.microsoft.com/admin/api/settings/security/guestUserPolicy"
-    $Output1 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    #$Uri = "https://admin.microsoft.com/admin/api/settings/security/guestUserPolicy"
+    #$Output1 = Invoke-O365Admin -Uri $Uri -Headers $Headers
 
 
     $Uri = "https://admin.microsoft.com/admin/api/settings/security/o365guestuser"
     $Output2 = Invoke-O365Admin -Uri $Uri -Headers $Headers
 
-    <# same/similar properties on both objects
-    $Object = [ordered] @{}
-    foreach ($O in $Output1, $Output2) {
-        foreach ($Key in $O.PSObject.Properties.Name) {
-            $Object[$Key] = $O.$Key
-        }
-    }
-     [PSCustomObject] $Object
-    #>
-
     [PSCustomObject] @{
-        AllowGuestAccess      = $Output1.AllowGuestAccess
-        AllowGuestInvitations = $Output1.AllowGuestInvitations
-        SitesSharingEnabled   = $Output1.SitesSharingEnabled
-        AllowGuestsAsMembers  = $Output2.AllowGuestsAsMembers
-        AllowGuestAccessO365  = $Output2.AllowGuestAccess
+        #AllowGuestAccess      = $Output1.AllowGuestAccess
+        #AllowGuestInvitations = $Output1.AllowGuestInvitations
+        #SitesSharingEnabled   = $Output1.SitesSharingEnabled
+        AllowGuestsAsMembers = $Output2.AllowGuestsAsMembers
+        AllowGuestAccess     = $Output2.AllowGuestAccess
     }
 }
---
+++ Public/Set-O365Groups.ps1
@@ -0,0 +1,23 @@
+﻿function Set-O365Groups {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $AllowGuestAccess,
+        [nullable[bool]] $AllowGuestsAsMembers
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/o365guestuser"
+
+    $CurrentSettings = Get-O365Groups -Headers $Headers
+    $Body = [ordered] @{
+        AllowGuestAccess     = $CurrentSettings.AllowGuestAccess
+        AllowGuestsAsMembers = $CurrentSettings.AllowGuestsAsMembers
+    }
+    if ($null -ne $AllowGuestAccess) {
+        $Body.AllowGuestAccess = $AllowGuestAccess
+    }
+    if ($null -ne $AllowGuestsAsMembers) {
+        $Body.AllowGuestsAsMembers = $AllowGuestsAsMembers
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    $Output
+}
