042f0591af398feebb4447c400802ae15fe8c3c3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-21 11:06:55 +0200

    Small update with tests

--- Public/Invoke-O365Admin.ps1
+++ Public/Invoke-O365Admin.ps1
@@ -23,6 +23,16 @@
         Method      = $Method
         ContentType = $ContentType
     }
+    #$RestSplat.Headers."x-ms-mac-hosting-app" = 'M365AdminPortal'
+    #$RestSplat.Headers."x-ms-mac-version" = 'host-mac_2021.8.16.1'
+    #$RestSplat.Headers."sec-ch-ua" = '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"'
+    #$RestSplat.Headers."x-portal-routekey" = 'weu'
+    #$RestSplat.Headers."x-ms-mac-appid" = 'feda2aab-4737-4646-a86c-98a7742c70e6'
+    #$RestSplat.Headers."x-adminapp-request" = '/Settings/Services/:/Settings/L1/Whiteboard'
+    #$RestSplat.Headers."x-ms-mac-target-app" = 'MAC'
+    #$RestSplat.UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73'
+    #$RestSplat.Headers.Cookie = 'MC1=GUID=480c128a5ba04faea7df151a53bdfa9a&HASH=480c&LV=202107&V=4&LU=1627670649689'
+
     if ($Body) {
         $RestSplat['Body'] = $Body | ConvertTo-Json -Depth 5
     }
