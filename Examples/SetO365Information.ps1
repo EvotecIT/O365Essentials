e540eccb71bd42c1a4d287fdf6fcd30c38b82fa0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-21 11:07:03 +0200

    Update of example

--- Examples/SetO365Information.ps1
+++ Examples/SetO365Information.ps1
@@ -5,6 +5,7 @@ if (-not $Credentials) {
 }
 # This makes a connection to Office 365 tenant
 # since we don't want to save the data we null it out
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
 $null = Connect-O365Admin -Verbose -Credential $Credentials
 
 Set-O365Planner -AllowCalendarSharing $true
