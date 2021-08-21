ed307d9ce3b36773bae5ad03890f4189d6993f17
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-21 11:30:06 +0200

    Update cmdlets

--- Public/Get-O365BriefingEmail.ps1
+++ Public/Get-O365BriefingEmail.ps1
@@ -1,4 +1,20 @@
 ﻿function Get-O365BriefingEmail {
+    <#
+    .SYNOPSIS
+    Gets status of Briefing emails.
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Headers
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
@@ -6,5 +22,10 @@
 
     $Uri = "https://admin.microsoft.com/admin/api/services/apps/briefingemail"
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
+    if ($Output) {
+        [PSCustomObject] @{
+            IsMailEnabled         = $Output.IsMailEnabled
+            IsSubscribedByDefault = $Output.IsSubscribedByDefault
+        }
+    }
 }
