a8c4ba011147ef6c5686b07e322d7362b75ff7e3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-21 14:55:33 +0200

    New cmdlets

---
+++ Public/Set-O365Cortana.ps1
@@ -0,0 +1,33 @@
+﻿function Set-O365Cortana {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Headers
+    Parameter description
+
+    .PARAMETER Enabled
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [bool] $Enabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/services/apps/cortana"
+
+    $Body = @{
+        Enabled = $Enabled
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    $Output
+}
---
+++ Public/Set-O365Dynamics365SalesInsights.ps1
@@ -0,0 +1,14 @@
+﻿function Set-O365Dynamics365SalesInsights {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [bool] $ServiceEnabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/apps/dci"
+
+    $Body = @{
+        ServiceEnabled = $ServiceEnabled
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    $Output
+}
