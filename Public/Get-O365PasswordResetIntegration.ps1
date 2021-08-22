708475e494eab973324625e0368355384123f53d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-22 18:14:33 +0200

    Add cmdlet

---
+++ Public/Get-O365PasswordResetIntegration.ps1
@@ -0,0 +1,18 @@
+﻿function Get-O365PasswordResetIntegration {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+
+    #$Uri = "https://main.iam.ad.ext.azure.com/api/PasswordReset/IsOnPremisesPasswordResetAvailable"
+    $Uri = "https://main.iam.ad.ext.azure.com/api/PasswordReset/OnPremisesPasswordResetPolicies"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($Output) {
+        [PSCustomObject] @{
+            PasswordWritebackSupported = $Output.passwordWritebackSupported
+            # This one doesn't change and stays enabled all the time
+            #accountUnlockSupported     = $Output.accountUnlockSupported
+            AccountUnlockEnabled       = $Output.accountUnlockEnabled
+        }
+    }
+}
