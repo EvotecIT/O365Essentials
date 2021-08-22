3aee45e5e1a210354c629150aed6a6756eefc29f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-22 18:14:13 +0200

    Add cmdlet

---
+++ Public/Get-O365PasswordReset.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365PasswordReset {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://main.iam.ad.ext.azure.com/api/PasswordReset/PasswordResetPolicies"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
