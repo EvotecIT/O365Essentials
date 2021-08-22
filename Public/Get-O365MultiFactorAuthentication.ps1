21cdb91bf44a524b7eeedf465d7965dfc3fed76d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-22 17:53:37 +0200

    Added additional cmdlet

---
+++ Public/Get-O365MultiFactorAuthentication.ps1
@@ -0,0 +1,27 @@
+﻿function Get-O365MultiFactorAuthentication {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Headers
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    Based on: https://portal.azure.com/#blade/Microsoft_AAD_IAM/MultifactorAuthenticationMenuBlade/GettingStarted/fromProviders/
+
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    #$Uri = "https://main.iam.ad.ext.azure.com/api/MultiFactorAuthentication/GetOrCreateExpandedTenantModel?tenantName=Evotec"
+    $Uri = "https://main.iam.ad.ext.azure.com/api/MultiFactorAuthentication/GetOrCreateExpandedTenantModel"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
