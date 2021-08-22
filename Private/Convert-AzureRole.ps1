252ea69e272b7ab38c3fc112d535ef7bc37db5ce
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-22 15:11:34 +0200

    Private functions

---
+++ Private/Convert-AzureRole.ps1
@@ -0,0 +1,18 @@
+﻿function Convert-AzureRole {
+    [cmdletbinding()]
+    param(
+        [string[]] $RoleID
+    )
+    $Roles = [ordered] @{
+        '729827e3-9c14-49f7-bb1b-9608f156bbb8' = 'Helpdesk admin'
+        '62e90394-69f5-4237-9190-012177145e10' = 'Global Administrator'
+    }
+    foreach ($Role in $RoleID) {
+        $RoleName = $Roles[$Role]
+        if ($RoleName) {
+            $RoleName
+        } else {
+            $Role
+        }
+    }
+}
---
+++ Private/Convert-ContractType.ps1
@@ -0,0 +1,17 @@
+﻿function Convert-ContractType {
+    [cmdletbinding()]
+    param(
+        [string[]] $ContractType
+    )
+    $ContractTypeInformation = [ordered] @{
+        '3' = 'Reseller'
+    }
+    foreach ($Contract in $ContractType) {
+        $ContractName = $ContractTypeInformation[$Contract]
+        if ($ContractName) {
+            $ContractName
+        } else {
+            $Contract
+        }
+    }
+}
