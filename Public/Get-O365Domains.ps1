d92d6550fe46d2cb3bccfdd9de24072ee6c238fa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-22 17:53:15 +0200

    Added cmdlet

---
+++ Public/Get-O365Domains.ps1
@@ -0,0 +1,11 @@
+﻿function Get-O365Domains {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+
+    #$Uri = "https://admin.microsoft.com/admin/api/Domains/List?filter=&searchText=&computeDomainRegistrationData=true"
+    $Uri = "https://admin.microsoft.com/admin/api/Domains/List"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
