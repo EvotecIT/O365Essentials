4c63fba503cb2a4d41499a530d627b1b1f51fc26
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-24 23:49:47 +0200

    Updated examples

---
+++ Examples/GetO365ModernAuthentication.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+if (-not $Credentials) {
+    $Credentials = Get-Credential
+}
+# This makes a connection to Office 365 tenant, using credentials
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+$null = Connect-O365Admin -Verbose -Credential $Credentials
+
+Get-O365ModernAuthentication -Verbose
+
+Set-O365ModernAuthentication -Verbose -AllowBasicAuthPop $false -AllowBasicAuthImap $false -WhatIf
