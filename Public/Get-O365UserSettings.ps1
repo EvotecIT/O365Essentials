2e5c1794f85f267244b0841f2503c87d1f95559c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-24 20:10:09 +0200

    Added another cmdlet

---
+++ Public/Get-O365UserSettings.ps1
@@ -0,0 +1,10 @@
+﻿function Get-O365UserSettings {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+
+    $Uri = "https://main.iam.ad.ext.azure.com/api/Directories/Properties"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method GET
+    $Output
+}
