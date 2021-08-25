bccc2fa41bef4a13870af13b0a26389fd3316aa1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-25 20:54:30 +0200

    Renames

--- Public/Get-O365UserConsentApps.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Get-O365UserConsentApps {
-    [cmdletbinding()]
-    param(
-        [alias('Authorization')][System.Collections.IDictionary] $Headers
-    )
-    $Uri = "https://admin.microsoft.com/admin/api/settings/apps/IntegratedApps"
-    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
-}
