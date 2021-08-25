a60f52d1919fab11a493675db662cac13d392016
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-25 20:54:45 +0200

    new fucntions

---
+++ Public/Get-O365Group.ps1
@@ -0,0 +1,27 @@
+﻿function Get-O365Group {
+    [cmdletBinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [string] $Id,
+        [string[]] $Property,
+        [string] $Filter,
+        [string] $OrderBy
+    )
+    if ($ID) {
+        # Query a single group
+        $Uri = "https://graph.microsoft.com/v1.0/groups/$ID"
+        $QueryParameter = @{
+            '$Select' = $Property -join ','
+        }
+    } else {
+        # Query multiple groups
+        $Uri = 'https://graph.microsoft.com/v1.0/groups'
+        $QueryParameter = @{
+            '$Select'  = $Property -join ','
+            '$filter'  = $Filter
+            '$orderby' = $OrderBy
+        }
+    }
+    Remove-EmptyValue -Hashtable $QueryParameter
+    Invoke-O365Admin -Uri $Uri -Headers $Headers -QueryParameter $QueryParameter
+}
---
+++ Public/Get-O365GroupMember.ps1
@@ -0,0 +1,26 @@
+﻿function Get-O365GroupMember {
+    [cmdletBinding()]
+    param(
+        [parameter()][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter(Mandatory)][string] $Id,
+        [string] $Search,
+        [string[]] $Property
+    )
+    if ($ID) {
+        # Query a single group
+        $Uri = "https://graph.microsoft.com/v1.0/groups/$ID/members"
+        $QueryParameter = @{
+            '$Select' = $Property -join ','
+            '$Search' = $Search
+
+        }
+        if ($QueryParameter.'$Search') {
+            # This is required for search to work
+            # https://developer.microsoft.com/en-us/identity/blogs/build-advanced-queries-with-count-filter-search-and-orderby/
+            $Headers['ConsistencyLevel'] = 'eventual'
+        }
+
+        Remove-EmptyValue -Hashtable $QueryParameter
+        Invoke-O365Admin -Uri $Uri -Headers $Headers -QueryParameter $QueryParameter
+    }
+}
