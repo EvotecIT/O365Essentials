1262e9719cb129aa925e42399fb25375785464b9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-25 20:31:20 +0200

    Renamed function

---
+++ Public/Get-O365OrgM365Groups.ps1
@@ -0,0 +1,34 @@
+﻿function Get-O365OrgM365Groups {
+    <#
+    .SYNOPSIS
+    Choose how guests from outside your organization can collaborate with your users in Microsoft 365 Groups. Learn more about guest access to Microsoft 365 Groups
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Headers
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    #$Uri = "https://admin.microsoft.com/admin/api/settings/security/guestUserPolicy"
+    #$Output1 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/o365guestuser"
+    $Output2 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+
+    [PSCustomObject] @{
+        #AllowGuestAccess      = $Output1.AllowGuestAccess
+        #AllowGuestInvitations = $Output1.AllowGuestInvitations
+        #SitesSharingEnabled   = $Output1.SitesSharingEnabled
+        AllowGuestsAsMembers = $Output2.AllowGuestsAsMembers
+        AllowGuestAccess     = $Output2.AllowGuestAccess
+    }
+}
---
+++ Public/Set-O365OrgM365Groups.ps1
@@ -0,0 +1,45 @@
+﻿function Set-O365OrgM365Groups {
+    <#
+    .SYNOPSIS
+    Choose how guests from outside your organization can collaborate with your users in Microsoft 365 Groups. Learn more about guest access to Microsoft 365 Groups
+
+    .DESCRIPTION
+    Choose how guests from outside your organization can collaborate with your users in Microsoft 365 Groups. Learn more about guest access to Microsoft 365 Groups
+
+    .PARAMETER Headers
+    Parameter description
+
+    .PARAMETER AllowGuestAccess
+    PLet group owners add people outside your organization to Microsoft 365 Groups as guests
+
+    .PARAMETER AllowGuestsAsMembers
+    Let guest group members access group content
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $AllowGuestAccess,
+        [nullable[bool]] $AllowGuestsAsMembers
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/o365guestuser"
+
+    $CurrentSettings = Get-O365OrgM365Groups -Headers $Headers
+    $Body = [ordered] @{
+        AllowGuestAccess     = $CurrentSettings.AllowGuestAccess
+        AllowGuestsAsMembers = $CurrentSettings.AllowGuestsAsMembers
+    }
+    if ($null -ne $AllowGuestAccess) {
+        $Body.AllowGuestAccess = $AllowGuestAccess
+    }
+    if ($null -ne $AllowGuestsAsMembers) {
+        $Body.AllowGuestsAsMembers = $AllowGuestsAsMembers
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    $Output
+}
