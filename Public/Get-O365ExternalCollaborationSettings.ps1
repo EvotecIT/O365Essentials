c40ec089829179f04e5d3fa0e21a689ced38cbd2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-25 19:48:50 +0200

    Query over graph

---
+++ Public/Get-O365ExternalCollaborationSettings.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365ExternalCollaborationSettings {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    #$Uri = 'https://graph.microsoft.com/beta/policies/authorizationPolicy'
+    $Uri = 'https://graph.microsoft.com/v1.0/policies/authorizationPolicy'
+    Invoke-O365Admin -Uri $Uri -Headers $Headers
+}
