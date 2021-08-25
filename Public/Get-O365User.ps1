886445c6ab096a0c537b643c59eecb0edf0d2ab9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-25 21:44:33 +0200

    Improved cmdlets

--- Public/Get-O365User.ps1
+++ Public/Get-O365User.ps1
@@ -1,56 +1,69 @@
-﻿function Get-O365Group {
-    [cmdletBinding(DefaultParameterSetName = 'Default')]
+﻿function Get-O365User {
+    [cmdletBinding(DefaultParameterSetName = "Default")]
     param(
         [parameter(ParameterSetName = 'Default')]
         [parameter(ParameterSetName = 'Filter')]
+        [parameter(ParameterSetName = 'GuestsOnly')]
         [parameter(ParameterSetName = 'EmailAddress')]
-        [parameter(ParameterSetName = 'DisplayName')]
+        [parameter(ParameterSetName = 'UserPrincipalName')]
         [parameter(ParameterSetName = 'Id')]
-        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter()][alias('Authorization')][System.Collections.IDictionary] $Headers,
 
         [parameter(ParameterSetName = 'Id')][string] $Id,
 
-        [parameter(ParameterSetName = 'DisplayName')][string] $DisplayName,
+        [parameter(ParameterSetName = 'UserPrincipalName')][string] $UserPrincipalName,
 
         [alias('Mail')][parameter(ParameterSetName = 'EmailAddress')][string] $EmailAddress,
 
         [parameter(ParameterSetName = 'Default')]
         [parameter(ParameterSetName = 'Filter')]
+        [parameter(ParameterSetName = 'GuestsOnly')]
         [parameter(ParameterSetName = 'EmailAddress')]
-        [parameter(ParameterSetName = 'DisplayName')]
+        [parameter(ParameterSetName = 'UserPrincipalName')]
         [parameter(ParameterSetName = 'Id')]
         [string[]] $Property,
 
         [parameter(ParameterSetName = 'Default')]
         [parameter(ParameterSetName = 'Filter')][string] $Filter,
 
+        [parameter(ParameterSetName = 'GuestsOnly')][switch] $GuestsOnly,
+
+        [parameter(ParameterSetName = 'GuestsOnly')]
         [parameter(ParameterSetName = 'Default')]
         [parameter(ParameterSetName = 'Filter')]
         [string] $OrderBy
     )
-    if ($DisplayName) {
-        $Uri = 'https://graph.microsoft.com/v1.0/groups'
+    if ($GuestsOnly) {
+        $Uri = 'https://graph.microsoft.com/v1.0/users'
+        $QueryParameter = @{
+            '$Select'  = $Property -join ','
+            '$filter'  = "userType eq 'Guest'"
+            '$orderby' = $OrderBy
+        }
+    } elseif ($UserPrincipalName) {
+        $Uri = 'https://graph.microsoft.com/v1.0/users'
         $QueryParameter = @{
             '$Select' = $Property -join ','
-            '$filter' = "displayName eq '$DisplayName'"
+            '$filter' = "userPrincipalName eq '$UserPrincipalName'"
         }
     } elseif ($EmailAddress) {
-        $Uri = 'https://graph.microsoft.com/v1.0/groups'
+        $Uri = 'https://graph.microsoft.com/v1.0/users'
         $QueryParameter = @{
             '$Select' = $Property -join ','
             '$filter' = "mail eq '$EmailAddress'"
         }
     } elseif ($ID) {
         # Query a single group
-        $Uri = "https://graph.microsoft.com/v1.0/groups/$ID"
+        $Uri = "https://graph.microsoft.com/v1.0/users/$ID"
         $QueryParameter = @{
             '$Select' = $Property -join ','
         }
     } else {
         # Query multiple groups
-        $Uri = 'https://graph.microsoft.com/v1.0/groups'
+        $Uri = 'https://graph.microsoft.com/v1.0/users'
         $QueryParameter = @{
             '$Select'  = $Property -join ','
+            # https://docs.microsoft.com/en-us/graph/query-parameters#filter-parameter
             '$filter'  = $Filter
             '$orderby' = $OrderBy
         }
