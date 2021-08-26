f2c34e3867d88f5b3f8d8e69a2fe2905d48a0d02
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-26 19:20:33 +0200

    Not working functions

---
+++ Examples/SetO365InformationEndUserCommunication.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+if (-not $Credentials) {
+    $Credentials = Get-Credential
+}
+# This makes a connection to Office 365 tenant
+# since we don't want to save the data we null it out
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+$null = Connect-O365Admin -Verbose -Credential $Credentials
+
+# Doesn't work!!
+Get-O365CommunicationToUsers
+Set-O365CommunicationToUsers -ServiceEnabled $true -WhatIf
+Get-O365CommunicationToUsers
