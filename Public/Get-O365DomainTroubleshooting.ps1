3c58da30d38aaa74c4af5a96b19f16a020955dac
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-26 09:26:20 +0200

    added cmdlet

---
+++ Public/Get-O365DomainTroubleshooting.ps1
@@ -0,0 +1,17 @@
+﻿function Get-O365DomainTroubleshooting {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter(Mandatory)][string] $DomainName
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/Domains/CheckIsTroubleshootingAllowed"
+
+    $QueryParameter = @{
+        'domainName'             = $DomainName
+        #'overrideSkip'           = $true
+        'canRefreshCache'        = $true
+        #'dnsHealthCheckScenario' = 2
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -QueryParameter $QueryParameter
+    $Output
+}
