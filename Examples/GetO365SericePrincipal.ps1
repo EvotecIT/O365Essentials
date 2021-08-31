672be083d2edbaf0182f1d700d41884e763d0163
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-08-31 18:26:41 +0200

    Additional cmdlet

---
+++ Examples/GetO365SericePrincipal.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+if (-not $Credentials) {
+    $Credentials = Get-Credential
+}
+# This makes a connection to Office 365 tenant
+# since we don't want to save the data we null it out
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+$null = Connect-O365Admin -Verbose -Credential $Credentials
+
+Get-O365ServicePrincipal | Format-Table
+
+Get-O365ServicePrincipal -Id '2f4f683a-7adf-41c4-8ca5-58ef1991b17d' | Format-Table
+
+Get-O365ServicePrincipal -ServicePrincipalType Legacy | Format-Table
