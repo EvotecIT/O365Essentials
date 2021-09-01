d3822e862deecf7b08be0c5046c7bf4933f6d004
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-01 22:38:12 +0200

    Works now

---
+++ Public/Set-O365BillingNotifications.ps1
@@ -0,0 +1,35 @@
+﻿function Set-O365BillingNotifications {
+    <#
+    .SYNOPSIS
+    Sets settijngs for Billing notifications (Invoice PDF ON/OFF)
+
+    .DESCRIPTION
+    Sets settijngs for Billing notifications (Invoice PDF ON/OFF)
+
+    .PARAMETER Headers
+    Parameter description
+
+    .PARAMETER SendInvoiceEmails
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    Sets settings for Billing notifications https://admin.microsoft.com/#/BillingNotifications
+    #>
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter(Mandatory)][bool] $SendInvoiceEmails
+    )
+    $Uri = "https://admin.microsoft.com/fd/commerceMgmt/mgmtsettings/invoicePreference?api-version=1.0"
+
+    $Body = @{
+        sendInvoiceEmails = $SendInvoiceEmails
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    if ($Output.setInvoicePreferenceSuccessful -eq $true) {
+
+    }
+}
