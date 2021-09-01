f7345a0cefbf10f94131da252283acfb05144c92
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-01 17:18:12 +0200

    requires scopes

---
+++ Public/Get-O365ExternalCollaborationFlows.ps1
@@ -0,0 +1,27 @@
+﻿function Get-O365ExternalCollaborationFlows {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Headers
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    WARNING: Invoke-O365Admin - Error JSON: Response status code does not indicate success:
+    403 (Forbidden). The application does not have any of the required delegated permissions
+    (Policy.Read.All, Policy.ReadWrite.AuthenticationFlows) to access the resource.
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = 'https://graph.microsoft.com/v1.0/policies/authenticationFlowsPolicy'
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
