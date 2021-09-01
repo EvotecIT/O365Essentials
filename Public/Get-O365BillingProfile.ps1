f6055868c70a08865ded81dacb1a97ef1e820c05
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-01 22:37:58 +0200

    Still doesn't work

---
+++ Public/Get-O365BillingProfile.ps1
@@ -0,0 +1,18 @@
+﻿function Get-O365BillingProfile {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+
+    $Uri = "https://admin.microsoft.com/fd/commerceMgmt/moderncommerce/checkaccess/bulk?api-version=3.0&accountId=91e58......."
+
+    $Body = @{
+        "permissionId"       = "40000000-aaaa-bbbb-ccc............"
+        "organizationId"     = "19419c1b-1bf1-41...."
+        "commerceObjectType" = "BillingGroup"
+        "commerceObjectId"   = "6YPQ-QFKZ....."
+    }
+
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    $Output
+}
