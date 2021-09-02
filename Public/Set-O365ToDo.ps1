c99505e6207106259714a7e54db737ba63991234
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-02 10:50:45 +0200

    Working cmdlet

---
+++ Public/Set-O365ToDo.ps1
@@ -0,0 +1,31 @@
+﻿function Set-O365Todo {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $ExternalJoinEnabled,
+        [nullable[bool]] $PushNotificationEnabled,
+        [nullable[bool]] $ExternalShareEnabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/services/apps/todo"
+
+    $CurrentSettings = Get-O365ToDo
+    if ($CurrentSettings) {
+        $Body = @{
+            IsExternalJoinEnabled     = $CurrentSettings.IsExternalJoinEnabled
+            IsPushNotificationEnabled = $CurrentSettings.IsPushNotificationEnabled
+            IsExternalShareEnabled    = $CurrentSettings.IsExternalShareEnabled
+        }
+        if ($null -ne $ExternalJoinEnabled) {
+            $Body.IsExternalJoinEnabled = $ExternalJoinEnabled
+        }
+        if ($null -ne $PushNotificationEnabled) {
+            $Body.IsPushNotificationEnabled = $PushNotificationEnabled
+        }
+        if ($null -ne $ExternalShareEnabled) {
+            $Body.IsExternalShareEnabled = $ExternalShareEnabled
+        }
+        Remove-EmptyValue -Hashtable $Body
+        $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+        $Output
+    }
+}
