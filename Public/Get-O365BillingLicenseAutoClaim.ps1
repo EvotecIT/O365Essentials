580b5fdf1c844c6d3303267a9502526c495fb5d3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-05 00:01:34 +0200

    Added more

---
+++ Public/Get-O365BillingLicenseAutoClaim.ps1
@@ -0,0 +1,14 @@
+﻿function Get-O365BillingLicenseAutoClaim {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    if ($Headers) {
+        $TentantID = $Headers.Tenant
+    } else {
+        $TentantID = $Script:AuthorizationO365Cache.Tenant
+    }
+    $Uri = "https://admin.microsoft.com/fd/m365licensing/v1/tenants/$TentantID/licenseddevicesassets"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output.items
+}
---
+++ Public/Get-O365BillingLicenseRequests.ps1
@@ -0,0 +1,14 @@
+﻿function Get-O365BillingLicenseRequests {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    if ($Headers) {
+        $TentantID = $Headers.Tenant
+    } else {
+        $TentantID = $Script:AuthorizationO365Cache.Tenant
+    }
+    $Uri = "https://admin.microsoft.com/fd/m365licensing/v1/tenants/$TentantID/self-service-requests"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output.items
+}
