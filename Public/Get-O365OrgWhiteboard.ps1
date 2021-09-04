e50c6702a5172b0338d78f33fcb0b97d92fa9cb5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 17:19:53 +0200

    Whiteboard updates

---
+++ Public/Get-O365OrgWhiteboard.ps1
@@ -0,0 +1,34 @@
+﻿function Get-O365OrgWhiteboard {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+
+    $TranslateTelemetry = @{
+        '0' = 'Neither'
+        '1' = 'Required'
+        '2' = 'Optional'
+    }
+    $Uri = 'https://admin.microsoft.com/admin/api/settings/apps/whiteboard'
+
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($NoTranslation) {
+        $Output
+    } else {
+        if ($Output) {
+            [PSCustomObject] @{
+                WhiteboardEnabled            = $Output.IsEnabled
+                DiagnosticData               = $TranslateTelemetry[$Output.TelemetryPolicy.ToString()]
+                OptionalConnectedExperiences = $Output.AreConnectedServicesEnabled
+                BoardSharingEnabled          = $Output.IsClaimEnabled
+                OneDriveStorageEnabled       = $Output.IsSharePointDefault
+                # Not sure what this does
+                NonTenantAccess              = $Output.NonTenantAccess
+                #LearnMoreUrl                 = $Output.LearnMoreUrl
+                #ProductUrl                   = $Output.ProductUrl
+                #TermsOfUseUrl                = $Output.TermsOfUseUrl
+            }
+        }
+    }
+}
---
+++ Public/Set-O365OrgWhiteboard.ps1
@@ -0,0 +1,45 @@
+﻿function Set-O365OrgWhiteboard {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $WhiteboardEnabled,
+        [ValidateSet('Neither', 'Required', 'Optional')]$DiagnosticData,
+        [nullable[bool]] $OptionalConnectedExperiences,
+        [nullable[bool]] $BoardSharingEnabled,
+        [nullable[bool]] $OneDriveStorageEnabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/apps/whiteboard"
+
+    $CurrentSettings = Get-O365OrgWhiteboard -Headers $Headers -NoTranslation
+
+    $Body = [ordered] @{
+        IsEnabled                   = $CurrentSettings.IsEnabled                   # : True
+        IsClaimEnabled              = $CurrentSettings.IsClaimEnabled              #: True
+        IsSharePointDefault         = $CurrentSettings.IsSharePointDefault         #: False
+        # This always seems to be 0, but i'll let it read it from Get-O365OrgWhiteboard
+        NonTenantAccess             = $CurrentSettings.NonTenantAccess             #: 0
+        TelemetryPolicy             = $CurrentSettings.TelemetryPolicy             #: 2
+        AreConnectedServicesEnabled = $CurrentSettings.AreConnectedServicesEnabled #: True
+    }
+    if ($null -ne $WhiteboardEnabled) {
+        $Body.IsEnabled = $WhiteboardEnabled
+    }
+    if ($DiagnosticData) {
+        $ReverseTranslateTelemetry = @{
+            'Neither'  = 0
+            'Required' = 1
+            'Optional' = 2
+        }
+        $Body.TelemetryPolicy = $ReverseTranslateTelemetry[$DiagnosticData]
+    }
+    if ($null -ne $OptionalConnectedExperiences) {
+        $Body.AreConnectedServicesEnabled = $OptionalConnectedExperiences
+    }
+    if ($null -ne $BoardSharingEnabled) {
+        $Body.IsClaimEnabled = $BoardSharingEnabled
+    }
+    if ($null -ne $OneDriveStorageEnabled) {
+        $Body.IsSharePointDefault = $OneDriveStorageEnabled
+    }
+    $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+}
