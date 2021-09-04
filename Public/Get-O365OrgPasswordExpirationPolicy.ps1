4b8c9c12d53bc170e8d6d94379dcef98769731d3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 17:52:44 +0200

    Expiration policy updates

---
+++ Public/Get-O365OrgPasswordExpirationPolicy.ps1
@@ -0,0 +1,24 @@
+﻿function Get-O365OrgPasswordExpirationPolicy {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+
+    $Uri = "https://admin.microsoft.com/admin/api/Settings/security/passwordpolicy"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($NoTranslation) {
+        $Output
+    } else {
+        [PSCustomObject] @{
+            PasswordNeverExpires      = $Output.NeverExpire
+            DaysBeforePasswordExpires = $Output.ValidityPeriod
+            DaysBeforeUserNotified    = $Output.NotificationDays
+            # not shown in the GUI
+            # MinimumValidityPeriod   : 14
+            # MinimumNotificationDays : 1
+            # MaximumValidityPeriod   : 730
+            # MaximumNotificationDays : 30
+        }
+    }
+}
---
+++ Public/Set-O365OrgPasswordExpirationPolicy.ps1
@@ -0,0 +1,29 @@
+﻿function Set-O365OrgPasswordExpirationPolicy {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [Parameter()][nullable[bool]] $PasswordNeverExpires,
+        [Parameter()][nullable[int]] $DaysBeforePasswordExpires,
+        [Parameter()][nullable[int]] $DaysBeforeUserNotified
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/Settings/security/passwordpolicy"
+
+    $CurrentSettings = Get-O365OrgPasswordExpirationPolicy -Headers $Headers -NoTranslation
+    if ($CurrentSettings) {
+        $Body = @{
+            ValidityPeriod   = $CurrentSettings.ValidityPeriod   #: 90
+            NotificationDays = $CurrentSettings.NotificationDays #: 14
+            NeverExpire      = $CurrentSettings.NeverExpire      #: True
+        }
+        if ($null -ne $DaysBeforeUserNotified) {
+            $Body.NotificationDays = $DaysBeforeUserNotified
+        }
+        if ($null -ne $DaysBeforePasswordExpires) {
+            $Body.ValidityPeriod = $DaysBeforePasswordExpires
+        }
+        if ($null -ne $PasswordNeverExpires) {
+            $Body.NeverExpire = $PasswordNeverExpires
+        }
+        $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    }
+}
