dc3c4a013ca0615d5e5a33b3b7bdffbceef341ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 22:41:37 +0200

    New cmdlets

---
+++ Public/Get-O365OrgProject.ps1
@@ -0,0 +1,20 @@
+﻿function Get-O365OrgProject {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+
+    $Uri = "https://admin.microsoft.com/admin/api/settings/apps/projectonline"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($NoTranslation) {
+        $Output
+    } else {
+        if ($Output) {
+            [PSCustomObject] @{
+                RoadmapEnabled          = $Output.IsRoadmapEnabled
+                ProjectForTheWebEnabled = $Output.IsModProjEnabled
+            }
+        }
+    }
+}
---
+++ Public/Set-O365OrgProject.ps1
@@ -0,0 +1,27 @@
+﻿function Set-O365OrgProject {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $RoadmapEnabled,
+        [nullable[bool]] $ProjectForTheWebEnabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/apps/projectonline"
+
+    $CurrentSettings = Get-O365OrgProject -Headers $Headers -NoTranslation
+    if ($CurrentSettings) {
+        $Body = @{
+            IsRoadmapEnabled          = $CurrentSettings.IsRoadmapEnabled          #: True
+            IsModProjEnabled          = $CurrentSettings.IsModProjEnabled          #: True
+            RoadmapAvailabilityError  = $CurrentSettings.RoadmapAvailabilityError  #: 0
+            ModProjAvailabilityStatus = $CurrentSettings.ModProjAvailabilityStatus #: 0
+        }
+        if ($null -ne $RoadmapEnabled) {
+            $Body.IsRoadmapEnabled = $RoadmapEnabled
+        }
+        if ($null -ne $ProjectForTheWebEnabled) {
+            $Body.IsModProjEnabled = $ProjectForTheWebEnabled
+        }
+        $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+        $Output
+    }
+}
