f4bace8a186aa37cbb305f3d55da23a241588e6f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 22:04:58 +0200

    Require Approval

---
+++ Public/Get-O365OrgCustomerLockbox.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365OrgCustomerLockbox {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/dataaccess"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
---
+++ Public/Set-O365OrgCustomerLockbox.ps1
@@ -0,0 +1,12 @@
+﻿function Set-O365OrgCustomerLockbox {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [Parameter(Mandatory)][bool] $RequireApproval
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/dataaccess"
+    $Body = @{
+        RequireApproval = $RequireApproval
+    }
+    $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+}
