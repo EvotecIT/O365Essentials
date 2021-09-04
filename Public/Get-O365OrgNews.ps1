0fd7cc140ee0f2b47526ebf4e741ee0c42fc2163
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 23:29:47 +0200

    Improvements to News

--- Public/Get-O365OrgNews.ps1
+++ Public/Get-O365OrgNews.ps1
@@ -1,31 +1,19 @@
 ﻿function Get-O365OrgNews {
     [cmdletbinding()]
     param(
-        [alias('Authorization')][System.Collections.IDictionary] $Headers
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
     )
-    <#
-    $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/news"
-    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
-
-
-
     $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/news/options/Bing"
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
-
-    $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/news/industry/Bing"
-    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
-
-    $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/news/msbenabled/Bing"
-    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
-    #>
-
-    $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/news/options"
-    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
-
-    # WARNING: Invoke-O365Admin - Error JSON: Response status code does not indicate success: 400 (Bad Request). An API version is required, but was not specified.
+    if ($NoTranslation) {
+        $Output
+    } else {
+        If ($Output) {
+            [PSCustomObject] @{
+                ContentOnNewTabEnabled               = $Output.NewsOptions.EdgeNTPOptions.IsOfficeContentEnabled
+                CompanyInformationAndIndustryEnabled = $Output.NewsOptions.EdgeNTPOptions.IsShowCompanyAndIndustry
+            }
+        }
+    }
 }
---
+++ Public/Set-O365OrgNews.ps1
@@ -0,0 +1,25 @@
+﻿function Set-O365OrgNews {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $ContentOnNewTabEnabled,
+        [nullable[bool]] $CompanyInformationAndIndustryEnabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/news/options"
+
+    $CurrentSettings = Get-O365OrgNews -Headers $Headers -NoTranslation
+    if ($CurrentSettings) {
+        $Body = [ordered] @{
+            ServiceType = 'Bing'
+            NewsOptions = $CurrentSettings.NewsOptions
+        }
+        if ($null -ne $ContentOnNewTabEnabled) {
+            $Body.NewsOptions.EdgeNTPOptions.IsOfficeContentEnabled = $ContentOnNewTabEnabled
+        }
+        if ($null -ne $CompanyInformationAndIndustryEnabled) {
+            $Body.NewsOptions.EdgeNTPOptions.IsShowCompanyAndIndustry = $CompanyInformationAndIndustryEnabled
+        }
+        $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method PUT -Body $Body
+        $Output
+    }
+}
