616b5b63b696136d987c7c2fc380a1891e5ea427
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 18:52:46 +0200

    Org Sharing

---
+++ Public/Get-O365OrgSharing.ps1
@@ -0,0 +1,35 @@
+﻿function Get-O365OrgSharing {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/guestUserPolicy"
+    $Output1 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($NoTranslation) {
+        $Output1
+    } else {
+        # In fiddler we coudld see additional queries, but in edge/chrome not so much
+        #$Uri = "https://admin.microsoft.com/admin/api/settings/apps/sitessharing"
+        #$Output2 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+        #$Output2 | Format-Table
+
+        # $Uri = "https://admin.microsoft.co//admin/api/settings/security/o365guestuser"
+        # $Output3 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+        # $Output3 | Format-Table
+        if ($Output1) {
+            [PSCustomObject] @{
+                # GUI doesn't show them, so mayne lets not show them eiter
+                #AllowGuestAccess                  = $Output1.AllowGuestAccess
+                LetUsersAddNewGuests = $Output1.AllowGuestInvitations
+                #SitesSharingEnabled               = $Output1.SitesSharingEnabled
+                #AllowSharing                      = $Output2.AllowSharing
+                #SiteUrl                           = $Output2.SiteUrl
+                #AdminUri                          = $Output2.AdminUri
+                #RequireAnonymousLinksExpireInDays = $Output2.RequireAnonymousLinksExpireInDays
+                #CollaborationType                 = $Output2.CollaborationType
+            }
+        }
+    }
+}
---
+++ Public/Set-O365OrgSharing.ps1
@@ -0,0 +1,12 @@
+﻿function Set-O365OrgSharing {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [Parameter(Mandatory)][bool] $LetUsersAddNewGuests
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/security/guestUserPolicy"
+    $Body = @{
+        AllowGuestInvitations = $LetUsersAddNewGuests
+    }
+    $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+}
