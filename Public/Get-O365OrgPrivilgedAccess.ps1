064b58d61b8ff73bf8f4382b610ca1b194495761
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-04 23:38:08 +0200

    Priv Access

---
+++ Public/Get-O365OrgPrivilgedAccess.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365OrgPrivilegedAccess {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/Settings/security/tenantLockbox"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
---
+++ Public/Set-O365OrgPrivilegedAccess.ps1
@@ -0,0 +1,17 @@
+﻿function Set-O365OrgPrivilegedAccess {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [nullable[bool]] $TenantLockBoxEnabled,
+        [string] $AdminGroup
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/Settings/security/tenantLockbox"
+
+    $Body = @{
+        EnabledTenantLockbox = $TenantLockBoxEnabled
+        AdminGroup           = $AdminGroup
+        Identity             = $null
+    }
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    $Output
+}
