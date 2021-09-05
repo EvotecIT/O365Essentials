3d41e23ec4e08fa70a9b5828159053128067f481
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-05 22:44:41 +0200

    Added command

---
+++ Public/Get-O365AzureLicenses.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365AzureLicenses {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://main.iam.ad.ext.azure.com/api/AccountSkus?backfillTenants=false"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
