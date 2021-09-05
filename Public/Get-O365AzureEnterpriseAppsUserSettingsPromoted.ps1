347395d164ff1efa4cd9fb9eb893880944a9abde
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-05 18:44:26 +0200

    New cmdlet

---
+++ Public/Get-O365AzureEnterpriseAppsUserSettingsPromoted.ps1
@@ -0,0 +1,12 @@
+﻿function Get-O365AzureEnterpriseAppsUserSettingsPromoted {
+    # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/UserSettings/menuId/
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = 'https://main.iam.ad.ext.azure.com/api/workspaces/promotedapps'
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
