bd01d62598745ff19e2d3ad1a70bb12fa23146b5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-05 22:45:16 +0200

    Added

---
+++ Public/Get-O365GroupAdministrativeUnit.ps1
@@ -0,0 +1,17 @@
+﻿function Get-O365GroupAdministrativeUnit {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter()][string] $GroupID = '75233998-a950-41de-97d0-6c259d0580a7'
+    )
+
+    if ($GroupID) {
+        $Group = $GroupID
+    } elseif ($GroupDisplayName) {
+        $Group = $GroupDisplayName
+    }
+    #$Uri = "https://graph.microsoft.com/beta/groups/$Group/memberOf/microsoft.graph.administrativeUnit"
+    $Uri = "https://graph.microsoft.com/v1.0/groups/$Group/memberOf/microsoft.graph.administrativeUnit"
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
