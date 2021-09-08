44f91a7454ee6007b6d4d436cf87bb29d7e4825a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-08 18:38:22 +0200

    Update example

---
+++ Examples/GetO365AzureLicensing.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+if (-not $Credentials) {
+    $Credentials = Get-Credential
+}
+# This makes a connection to Office 365 tenant
+# since we don't want to save the data we null it out
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+$null = Connect-O365Admin -Verbose -Credential $Credentials
+
+$Licenses = Get-O365AzureLicenses
+$Licenses | Format-Table
+
+$ServicePlans = Get-O365AzureLicenses -ServicePlans -LicenseName 'Enterprise Mobility + Security E5' -Verbose
+$ServicePlans | Format-Table
+
+$ServicePlans = Get-O365AzureLicenses -ServicePlans -LicenseSKUID 'EMSPREMIUM' -Verbose
+$ServicePlans | Format-Table
+
+$ServicePlans = Get-O365AzureLicenses -ServicePlans -LicenseSKUID 'evotecpoland:EMSPREMIUM' -Verbose
+$ServicePlans | Format-Table
