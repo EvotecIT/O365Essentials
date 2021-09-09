33a12cfdaf11ca862f5e65ac2850964504434c9e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-09 23:14:42 +0200

    add example

--- Examples/GetO365AzureGroupLicensing.ps1
+++ Examples/GetO365AzureGroupLicensing.ps1
@@ -8,4 +8,8 @@ if (-not $Credentials) {
 # keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
 $null = Connect-O365Admin -Verbose -Credential $Credentials
 
-Get-O365AzureLicenses | Format-Table
+$LicenseGroup = Get-O365GroupLicenses -GroupID '75233998-a950-41de-97d0-6c259d0580a7' -Verbose
+$LicenseGroup | Format-Table
+
+$LicenseGroup = Get-O365GroupLicenses -GroupName 'Test-Group-TestEVOTECPL' -Verbose
+$LicenseGroup | Format-Table
