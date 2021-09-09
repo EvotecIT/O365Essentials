305c3dd0f7cea08f466c50e26ed4741c153651db
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-09 23:15:08 +0200

    add cmdlet

---
+++ Public/Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
@@ -0,0 +1,25 @@
+﻿function Get-O365AzureEnterpriseAppsUserSettingsAdmin {
+    # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/UserSettings/menuId/
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+    $Uri = 'https://main.iam.ad.ext.azure.com/api/RequestApprovals/V2/PolicyTemplates?type=AdminConsentFlow'
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($Output) {
+        if ($NoTranslation) {
+            $Output
+        } else {
+            [PSCustomObject] @{
+                requestExpiresInDays = $Output.requestExpiresInDays
+                notificationsEnabled = $Output.notificationsEnabled
+                remindersEnabled     = $Output.remindersEnabled
+                approvers            = $Output.approvers
+                approversV2          = $Output.approversV2
+            }
+        }
+    }
+}
