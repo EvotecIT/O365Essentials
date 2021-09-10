890030d4e4287e6273bc80a52dc566847ed3d2ed
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-10 11:34:34 +0200

    Added new cmdlet

---
+++ Public/Get-O365BillingSubscriptions.ps1
@@ -0,0 +1,23 @@
+﻿function Get-O365BillingSubscriptions {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [string[]]$Property,
+        [string] $OrderBy
+    )
+
+    #$Uri = "https://admin.microsoft.com/fd/commerceapi/my-org/subscriptions?`$filter=parentId%20eq%20null&`$expand=subscribedsku&optional=cspsubscriptions,price,actions,transitiondetails,quickstarttag"
+    $Uri = "https://admin.microsoft.com/fd/commerceapi/my-org/subscriptions"
+
+    $QueryParameter = @{
+        '$Select'  = $Property -join ','
+        '$filter'  = 'parentId eq null'
+        '$orderby' = $OrderBy
+        'expand'   = 'subscribedsku'
+        'optional' = "cspsubscriptions,price,actions,transitiondetails,quickstarttag"
+    }
+    Remove-EmptyValue -Hashtable $QueryParameter
+
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -QueryParameter $QueryParameter
+    $Output
+}
