96e788b22816690a3866069f910856ebb668a45c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-10 11:33:48 +0200

    Fixes for multiple partners

--- Public/Get-O365PartnerRelationship.ps1
+++ Public/Get-O365PartnerRelationship.ps1
@@ -16,16 +16,18 @@
         $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
 
         if ($Output.partners) {
-            [PSCustomObject] @{
-                id            = $Output.partners.id  #: c2248f0a
-                name          = $Output.partners.name  #:
-                aadRoles      = Convert-AzureRole -RoleID $Output.partners.aadRoles
-                companyType   = $Output.partners.companyType  #: 4
-                canRemoveDap  = $Output.partners.canRemoveDap  #: True
-                contractTypes = Convert-ContractType -ContractType $Output.partners.contractTypes  #: {3}
-                partnerType   = $Output.partners.partnerType  #: 1
+            foreach ($Partner in $Output.partners) {
+                [PSCustomObject] @{
+                    id            = $Partner.id  #: c2248f0a
+                    name          = $Partner.name  #:
+                    aadRoles      = Convert-AzureRole -RoleID $Partner.aadRoles
+                    # i am not 100% sure on the conversion types on different numbers so i'll disable them for now
+                    companyType   = $Partner.companyType #Convert-CompanyType -CompanyType $Partner.companyType  #: 4
+                    canRemoveDap  = $Partner.canRemoveDap  #: True
+                    contractTypes = $Partner.contractTypes # Convert-ContractType -ContractType $Partner.contractTypes  #: {3}
+                    partnerType   = $Partner.partnerType  #: 1
+                }
             }
-
         }
     } else {
         Write-Warning -Message "Get-O365PartnerRelationship - TenantID was not found in headers. Skipping."
