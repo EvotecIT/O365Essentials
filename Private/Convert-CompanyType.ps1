4714924d6637ee7237b5ed2c6bf2e36bc18fc776
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-10 11:33:37 +0200

    Company type added but probably not working

---
+++ Private/Convert-CompanyType.ps1
@@ -0,0 +1,18 @@
+﻿function Convert-CompanyType {
+    [cmdletbinding()]
+    param(
+        [string[]] $CompanyType
+    )
+    $CompanyTypeInformation = [ordered] @{
+        '5' = 'Indirect reseller'
+        '4' = 'Reseller'
+    }
+    foreach ($Company in $CompanyType) {
+        $CompanyName = $CompanyTypeInformation[$Company]
+        if ($CompanyName) {
+            $CompanyName
+        } else {
+            $Company
+        }
+    }
+}
