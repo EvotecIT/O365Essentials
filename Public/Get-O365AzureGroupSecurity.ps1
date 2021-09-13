1fb26efe498794442ffd8f6b9b97376d113380e1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-13 09:39:58 +0200

    Added new cmds

---
+++ Public/Get-O365AzureGroupSecurity.ps1
@@ -0,0 +1,39 @@
+﻿function Get-O365AzureGroupSecurity {
+    <#
+    .SYNOPSIS
+    Get settings for Security Groups - "Users can create security groups in Azure portals, API or PowerShell"
+
+    .DESCRIPTION
+    Get settings for Security Groups - "Users can create security groups in Azure portals, API or PowerShell"
+
+    .PARAMETER Headers
+    Authorization header as created by Connect-O365Admin. If not provided the function will try to fetch it from the current execution context.
+
+    .PARAMETER NoTranslation
+    Provides output without any translation. Mostly required for testing or during internal configuration.
+
+    .EXAMPLE
+    Get-O365AzureGroupSecurity -Verbose
+
+    .NOTES
+    https://portal.azure.com/#blade/Microsoft_AAD_IAM/GroupsManagementMenuBlade/General
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+    $Uri = 'https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy'
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method Get
+
+
+    if ($Output) {
+        if ($NoTranslation) {
+            $Output
+        } else {
+            [PSCustomObject] @{
+                "AllowedToCreateSecurityGroups" = $Output.defaultUserRolePermissions.allowedToCreateSecurityGroups
+            }
+        }
+    }
+}
---
+++ Public/Get-O365AzureGroupSelfService.ps1
@@ -0,0 +1,28 @@
+﻿function Get-O365AzureGroupSelfService {
+    <#
+    .SYNOPSIS
+    Gets Azure Groups Self Service information.
+
+    .DESCRIPTION
+    Gets Azure Groups Self Service information.
+
+    .PARAMETER Headers
+    Authorization header as created by Connect-O365Admin. If not provided the function will try to fetch it from the current execution context.
+
+    .EXAMPLE
+    Get-O365AzureGroupSelfService
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = 'https://main.iam.ad.ext.azure.com/api/Directories/SsgmProperties/'
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method Get
+
+    if ($Output) {
+        $Output
+    }
+}
