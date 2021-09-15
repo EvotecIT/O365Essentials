d373a348a7e9656043d863bf4e1c8916b99dc8e5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-15 11:08:05 +0200

    New cmds, updates to existing commands

--- Public/Get-O365SearchIntelligenceBingConfigurations.ps1
+++ Public/Get-O365SearchIntelligenceBingConfigurations.ps1
@@ -6,5 +6,17 @@
 
     $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/configurations"
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output
+
+    if ($Output) {
+        [PSCustomObject] @{
+            # GUI only allowes single change to all services at once - this means if one is TRUE else is TRUE
+            ServiceEnabled    = if ($Output.People -eq $true) { $true } else { $false }
+            People            = $Output.People
+            Groups            = $Output.Groups
+            Documents         = $Output.Documents
+            Yammer            = $Output.Yammer
+            Teams             = $Output.Teams
+            TenantState       = $Output.TenantState
+        }
+    }
 }
---
+++ Public/Set-O365SearchIntelligenceBingConfigurations.ps1
@@ -0,0 +1,37 @@
+﻿function Set-O365SearchIntelligenceBingConfigurations {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [bool] $ServiceEnabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/searchadminapi/configuration/update"
+
+    if ($PSBoundParameters.ContainsKey('ServiceEnabled')) {
+
+        $CurrentSettings = Get-O365SearchIntelligenceBingConfigurations -Headers $Headers
+        if ($CurrentSettings) {
+            $Body = @{
+                IsServiceEnabledStateChanged = $true
+                # GUI only allows a single change to all services at once
+                <#
+                ServiceEnabled               = $CurrentSettings.ServiceEnabled               #: False
+                People                       = $CurrentSettings.People                       #: False
+                Groups                       = $CurrentSettings.Groups                       #: False
+                Documents                    = $CurrentSettings.Documents                    #: False
+                Yammer                       = $CurrentSettings.Yammer                       #: False
+                Teams                        = $CurrentSettings.Teams                        #: False
+#>
+                ServiceEnabled               = $ServiceEnabled               #: False
+                People                       = $ServiceEnabled                       #: False
+                Groups                       = $ServiceEnabled                       #: False
+                Documents                    = $ServiceEnabled                    #: False
+                Yammer                       = $ServiceEnabled                       #: False
+                Teams                        = $ServiceEnabled                        #: False
+                SaveTenantSettings           = $true           #: True
+            }
+
+            $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+            $Output
+        }
+    }
+}
