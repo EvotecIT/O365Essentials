4ad78caeef0a675f15b61be6f71ad8e8dcb5e328
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-19 20:55:52 +0200

    Another SET command

--- Public/Get-O365OrgOfficeProductivity.ps1
+++ Public/Get-O365OrgOfficeProductivity.ps1
@@ -6,19 +6,21 @@
     $Uri = "https://admin.microsoft.com/admin/api/reports/productivityScoreCustomerOption"
     $Output1 = Invoke-O365Admin -Uri $Uri -Headers $Headers
 
-    $Uri = "https://admin.microsoft.com/admin/api/reports/productivityScoreConfig/GetProductivityScoreConfig"
-    $Output2 = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output2Json = $Output2.Output | ConvertFrom-Json
-    $Output1Json = $Output1.Output | ConvertFrom-Json
-    $Output = [PSCustomObject] @{
-        TenantId                  = $Output2Json.TenantId
-        ProductivityScoreSignedup = $Output2Json.ProductivityScoreSignedup
-        SignupUserPuid            = $Output2Json.SignupUserPuid
-        SignupTime                = $Output2Json.SignupTime
-        ReadyTime                 = $Output2Json.ReadyTime
-        ProductivityScoreOptedIn  = $Output1Json.ProductivityScoreOptedIn
-        OperationUserPuid         = $Output1Json.OperationUserPuid
-        OperationTime             = $Output1Json.OperationTime
+    #$Uri = "https://admin.microsoft.com/admin/api/reports/productivityScoreConfig/GetProductivityScoreConfig"
+    #$Output2 = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($Output1) {
+        #$Output2Json = $Output2.Output | ConvertFrom-Json
+        $Output1Json = $Output1.Output | ConvertFrom-Json
+        $Output = [PSCustomObject] @{
+            #TenantId                  = $Output2Json.TenantId
+            #ProductivityScoreSignedup = $Output2Json.ProductivityScoreSignedup
+            #SignupUserPuid            = $Output2Json.SignupUserPuid
+            #SignupTime                = $Output2Json.SignupTime
+            #ReadyTime                 = $Output2Json.ReadyTime
+            ProductivityScoreOptedIn = $Output1Json.ProductivityScoreOptedIn
+            OperationUserPuid        = $Output1Json.OperationUserPuid
+            OperationTime            = $Output1Json.OperationTime
+        }
+        $Output
     }
-    $Output
 }
---
+++ Public/Set-O365OrgOfficeProductivity.ps1
@@ -0,0 +1,14 @@
+﻿function Set-O365OrgOfficeProductivity {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [alias('ProductivityScoreOptedIn')][parameter(Mandatory)][bool] $Enabled
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/reports/productivityScoreCustomerOption"
+
+    $Body = @{
+        ProductivityScoreOptedIn = $Enabled
+        OperationTime            = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+    }
+    $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+}
