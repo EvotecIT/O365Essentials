44cb6517ea08bd209960430c432ab45e5a234e56
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-19 21:28:05 +0200

    Added commands

---
+++ Examples/GetO365OrgHelpdeskInformation.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+if (-not $Credentials) {
+    $Credentials = Get-Credential
+}
+# This makes a connection to Office 365 tenant
+# since we don't want to save the data we null it out
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+$null = Connect-O365Admin -Verbose -Credential $Credentials
+
+Get-O365OrgHelpdeskInformation -Verbose
+
+Set-O365OrgHelpdeskInformation -Verbose -CustomHelpDeskInformationEnabled $true -Title 'Evotec Support' -PhoneNumber '111111111' -WhatIf
+
+Set-O365OrgHelpdeskInformation -Verbose -CustomHelpDeskInformationEnabled $true -Title 'Evotec Support' -EmailAddress 'contact@evotec.pl' -PhoneNumber '111111111' -WhatIf
---
+++ Public/Set-O365OrgHelpdeskInformation.ps1
@@ -0,0 +1,54 @@
+﻿function Set-O365OrgHelpdeskInformation {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [alias('Enabled')][bool] $CustomHelpDeskInformationEnabled,
+        [string] $Title,
+        [string] $PhoneNumber,
+        [string] $EmailAddress,
+        [string] $SupportUrl,
+        [string] $SupportUrlTitle
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/Settings/company/helpdesk"
+
+    $CurrentSettings = Get-O365OrgHelpdeskInformation -Headers $Headers
+    if ($CurrentSettings) {
+
+        $Body = @{
+            "CustomSupportEnabled" = $CurrentSettings.CustomSupportEnabled
+            "Title"                = $CurrentSettings.Title
+
+            "PhoneEnabled"         = $CurrentSettings.PhoneEnabled
+            "PhoneNumber"          = $CurrentSettings.PhoneNumber
+
+            "EmailEnabled"         = $CurrentSettings.EmailEnabled
+            "EmailAddress"         = $CurrentSettings.EmailAddress
+
+            "UrlEnabled"           = $CurrentSettings.UrlEnabled
+            "SupportUrl"           = $CurrentSettings.SupportUrl
+            "SupportUrlTitle"      = $CurrentSettings.SupportUrlTitle
+        }
+
+        if ($PSBoundParameters.ContainsKey('CustomHelpDeskInformationEnabled')) {
+            $Body.CustomSupportEnabled = $CustomHelpDeskInformationEnabled
+        }
+        if ($PSBoundParameters.ContainsKey('Title')) {
+            $Body.Title = $Title
+        }
+        if ($PSBoundParameters.ContainsKey('PhoneNumber')) {
+            $Body.PhoneNumber = $PhoneNumber
+            $Body.PhoneEnabled = if ($PhoneNumber) { $true } else { $false }
+        }
+        if ($PSBoundParameters.ContainsKey('EmailAddress')) {
+            $Body.EmailEnabled = if ($EmailAddress) { $true } else { $false }
+            $Body.EmailAddress = $EmailAddress
+        }
+        if ($PSBoundParameters.ContainsKey('SupportUrl')) {
+            $Body.SupportUrlTitle = $SupportUrlTitle
+            $Body.SupportUrl = $SupportUrl
+            $Body.UrlEnabled = if ($SupportUrl) { $true } else { $false }
+        }
+        $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+        $Output
+    }
+}
