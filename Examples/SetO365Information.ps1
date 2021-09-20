11c516e4341fae60f1a0343297072e3efd1b6cd2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-20 11:27:23 +0200

    Update example

--- Examples/SetO365Information.ps1
+++ Examples/SetO365Information.ps1
@@ -8,11 +8,11 @@ if (-not $Credentials) {
 # keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
 $null = Connect-O365Admin -Verbose -Credential $Credentials
 
-Set-O365OrgPlanner -AllowCalendarSharing $true
-Set-O365OrgForms -InOrgFormsPhishingScanEnabled $true
-Set-O365OrgAzureSpeechServices -AllowTheOrganizationWideLanguageModel $false
-Set-O365OrgBriefingEmail -SubscribeByDefault $false
-Set-O365OrgCalendarSharing -SharingOption CalendarSharingFreeBusyReviewed
-Set-O365OrgCortana -Enabled $false
-Set-O365OrgDynamics365SalesInsights -ServiceEnabled $false
-Set-O365OrgGroups -AllowGuestAccess $true -AllowGuestsAsMembers $true
+Set-O365OrgPlanner -AllowCalendarSharing $true -WhatIf
+Set-O365OrgForms -InOrgFormsPhishingScanEnabled $true -WhatIf
+Set-O365OrgAzureSpeechServices -AllowTheOrganizationWideLanguageModel $false -WhatIf
+Set-O365OrgBriefingEmail -SubscribeByDefault $false -WhatIf
+Set-O365OrgCalendarSharing -SharingOption CalendarSharingFreeBusyReviewer -WhatIf
+Set-O365OrgCortana -Enabled $false -WhatIf
+Set-O365OrgDynamics365SalesInsights -ServiceEnabled $false -WhatIf
+Set-O365OrgM365Groups -AllowGuestAccess $true -AllowGuestsAsMembers $true -WhatIf
