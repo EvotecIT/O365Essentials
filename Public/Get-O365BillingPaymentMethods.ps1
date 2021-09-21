ec0b3245fc51f7581fe6596aca94012092507481
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-21 12:38:20 +0200

    Wrong payment methods url was used

--- Public/Get-O365BillingPaymentMethods.ps1
+++ Public/Get-O365BillingPaymentMethods.ps1
@@ -1,10 +1,16 @@
 ﻿function Get-O365BillingPaymentMethods {
     [cmdletbinding()]
     param(
-        [alias('Authorization')][System.Collections.IDictionary] $Headers
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [string] $ID
     )
 
-    $Uri = "https://admin.microsoft.com/fd/commerceapi/my-org/paymentInstruments('ObnETQAAAAABAACA')/unsettledCharges"
+    if ($ID) {
+        # $Uri = "https://admin.microsoft.com/fd/commerceapi/my-org/paymentInstruments($ID)/unsettledCharges"
+        $Uri = "https://admin.microsoft.com/fd/commerceapi/my-org/paymentInstruments($ID)"
+    } else {
+        $Uri = "https://admin.microsoft.com/fd/commerceapi/my-org/paymentInstruments"
+    }
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
     $Output
 }
