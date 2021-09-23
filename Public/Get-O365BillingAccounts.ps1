7d3f19efd60d0ff069f0094f5547499e1be9eba7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-23 22:52:04 +0200

    Not working

--- Public/Get-O365BillingAccounts.ps1
+++ Public/Get-O365BillingAccounts.ps1
@@ -3,8 +3,8 @@
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
     )
-
-    $Uri = "https://admin.microsoft.com/fd/commerceMgmt/moderncommerce/accountGraph?api-version=3.0"
+    $Uri = "https://admin.microsoft.com/fd/jarvisCM/my-org/profiles?type=organization"
+    $Uri = "https://admin.microsoft.com/fd/commerceMgmt/billingaccount"
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
     $Output
 }
