15c5d7f53b420a3fad6a63bf478eb9fda1dfaa83
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-10-26 21:47:28 +0200

    added edge site lists

---
+++ Public/Get-O365OrgMicrosoftEdgeSiteLists.ps1
@@ -0,0 +1,10 @@
+﻿function Get-O365OrgMicrosoftEdgeSiteLists {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = 'https://admin.microsoft.com/fd/edgeenterprisesitemanagement/api/shard'
+
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    $Output
+}
