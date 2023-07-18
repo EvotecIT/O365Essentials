86ac9cba875d27f5aa7be66f37f616043d5c937b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-18 21:05:33 +0200

    Disconnect function

---
+++ Public/Disconnect-O365Admin.ps1
@@ -0,0 +1,8 @@
+﻿function Disconnect-O365Admin {
+    [CmdletBinding()]
+    param(
+
+    )
+    # quick and dirty removal of cached data that should force disconnection
+    $Script:AuthorizationO365Cache = $null
+}
