dea9f4f774f2af0a222223182e1005184dde8472
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-18 21:05:58 +0200

    Add collaboration restrictions

---
+++ Public/Get-O365AzureExternalCollaborationRestrictions.ps1
@@ -0,0 +1,48 @@
+﻿function Get-O365AzureExternalCollaborationRestrictions {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [switch] $NoTranslation
+    )
+    $Uri = 'https://main.iam.ad.ext.azure.com/api/B2B/b2bPolicy'
+    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
+    if ($Output) {
+        if ($NoTranslation) {
+            $Output
+
+            <#
+            targetedDomains                     : {}
+            hasListEntries                      : True
+            isAllowlist                         : False
+            otpEnabled                          : False
+            adminConsentedForUsersIntoTenantIds : {}
+            noAADConsentForUsersFromTenantsIds  : {}
+            #>
+        } else {
+
+            if ($Output.isAllowlist -eq $true -and $Output.targetedDomains.count -eq 0) {
+                $CollaborationRestrictions = 'AllowAnyDomain'
+            } elseif ($Output.isAllowlist -eq $true -and $Output.targetedDomains.count -gt 0) {
+                $CollaborationRestrictions = 'AllowSpecifiedDomains'
+            } elseif ($Output.isAllowlist -eq $false -and $Output.targetedDomains.count -eq 0) {
+                # Won't really happen as microsoft doesn't allow to to have 0 domains for allowed domains
+                $CollaborationRestrictions = 'DisallowAnyDomain'
+            } elseif ($Output.isAllowlist -eq $false -and $Output.targetedDomains.count -gt 0) {
+                $CollaborationRestrictions = 'DisallowSpecifiedDomains'
+            } else {
+                # Won't really happen
+                $CollaborationRestrictions = 'Unknown'
+            }
+
+            [PSCustomObject] @{
+                CollaborationRestrictions = $CollaborationRestrictions
+                TargetedDomains           = $Output.targetedDomains                     #
+                #hasListEntries            = $Output.hasListEntries                      # : True
+                #isAllowlist               = $Output.isAllowlist                         # : False
+                #otpEnabled                          = $Output.otpEnabled                          # : False
+                #adminConsentedForUsersIntoTenantIds = $Output.adminConsentedForUsersIntoTenantIds # : {}
+                #noAADConsentForUsersFromTenantsIds  = $Output.noAADConsentForUsersFromTenantsIds  # : {}          # :
+            }
+        }
+    }
+}
---
+++ Public/Set-O365AzureExternalCollaborationRestrictions.ps1
@@ -0,0 +1,53 @@
+﻿function Set-O365AzureExternalCollaborationRestrictions {
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [ValidateSet('AllowAnyDomains', 'AllowSpecifiedDomains', 'DisallowSpecifiedDomains')][string] $CollaborationRestrictions,
+        [string[]] $TargetedDomains
+    )
+
+    $Uri = 'https://main.iam.ad.ext.azure.com/api/B2B/b2bPolicy'
+
+    # $Body = @{
+    #     targetedDomains                     = $UsersCanConsentAppsAccessingData
+    #     isAllowlist                         = $UsersCanAddGalleryAppsToMyApp
+    #     hasListEntries                      = $UsersCanOnlySeeO365AppsInPortal
+    #     otpEnabled                          = $otpEnabled
+    #     adminConsentedForUsersIntoTenantIds = @()
+    #     noAADConsentForUsersFromTenantsIds  = @()
+    # }
+
+    if ($CollaborationRestrictions -eq 'AllowAnyDomains') {
+        $Body = @{
+            targetedDomains = @()
+            isAllowlist     = $true
+            #hasListEntries                      = $true
+            #otpEnabled                          = $false
+            #adminConsentedForUsersIntoTenantIds = @()
+            #noAADConsentForUsersFromTenantsIds  = @()
+        }
+    } elseif ($CollaborationRestrictions -eq 'AllowSpecifiedDomains') {
+        $Body = @{
+            targetedDomains = @($TargetedDomains)
+            isAllowlist     = $true
+            #hasListEntries                      = $true
+            #otpEnabled                          = $false
+            #adminConsentedForUsersIntoTenantIds = @()
+            #noAADConsentForUsersFromTenantsIds  = @()
+        }
+    } elseif ($CollaborationRestrictions -eq 'DisallowSpecifiedDomains') {
+        $Body = @{
+            targetedDomains = @($TargetedDomains)
+            isAllowlist     = $false
+            #hasListEntries                      = $true
+            #otpEnabled                          = $false
+            #adminConsentedForUsersIntoTenantIds = @()
+            #noAADConsentForUsersFromTenantsIds  = @()
+        }
+    }
+
+    #Remove-EmptyValue -Hashtable $Body
+    if ($Body.Keys.Count -gt 0) {
+        $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST -Body $Body
+    }
+}
