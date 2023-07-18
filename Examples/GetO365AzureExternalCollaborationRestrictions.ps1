dbb9fb7fad9fe72d1512f0896b3a7eba89a84c52
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-18 21:32:43 +0200

    new example

---
+++ Examples/GetO365AzureExternalCollaborationRestrictions.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+if (-not $Credentials) {
+   # $Credentials = Get-Credential
+}
+# This makes a connection to Office 365 tenant
+# since we don't want to save the data we null it out
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+
+$null = Connect-O365Admin -Verbose #-Credential $Credentials
+
+Get-O365AzureExternalCollaborationRestrictions
+Set-O365AzureExternalCollaborationRestrictions -CollaborationRestrictions AllowAnyDomains -Verbose
+#Set-O365AzureExternalCollaborationRestrictions -CollaborationRestrictions DisallowSpecifiedDomains -TargetedDomains 'test.com' -Verbose
+#Set-O365AzureExternalCollaborationRestrictions -CollaborationRestrictions AllowSpecifiedDomains -TargetedDomains 'test.com' -Verbose
