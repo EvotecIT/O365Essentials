61f9f9679e0d0a9d6c8012a7e69f22a44d90cb6b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-19 19:23:32 +0200

    Add account linking

---
+++ Public/Get-O365OrgAccountLinking.ps1
@@ -0,0 +1,29 @@
+﻿function Get-O365OrgAccountLinking {
+    <#
+    .SYNOPSIS
+    Users who connect their Azure AD account with their MSA account can earn rewards points when they search on Bing. User searches are never shared.
+
+    .DESCRIPTION
+    Users who connect their Azure AD account with their MSA account can earn rewards points when they search on Bing. User searches are never shared.
+
+    .PARAMETER Headers
+    The headers to use for the request
+
+    .EXAMPLE
+    Get-O365OrgAccountLinking
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://admin.microsoft.com/fd/bfb/api/v3/office/switch/feature"
+    $OutputSettings = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method POST
+    if ($OutputSettings) {
+        [PSCustomObject] @{
+            AccountLinking = if ($OutputSettings.result -contains 'AccountLinking') { $true } else { $false }
+        }
+    }
+}
---
+++ Public/Set-O365OrgAccountLinking.ps1
@@ -0,0 +1,41 @@
+﻿function Set-O365OrgAccountLinking {
+    <#
+    .SYNOPSIS
+    Users who connect their Azure AD account with their MSA account can earn rewards points when they search on Bing.
+    This cmdlet allows to enable/disable this feature.
+
+    .DESCRIPTION
+    Users who connect their Azure AD account with their MSA account can earn rewards points when they search on Bing.
+    This cmdlet allows to enable/disable this feature.
+
+    .PARAMETER Headers
+    The headers to use for the request
+
+    .PARAMETER EnableExtension
+    Enable or disable the feature
+
+    .EXAMPLE
+    Set-O365OrgAccountLinking
+
+    .NOTES
+    General notes
+    #>
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [Parameter(Mandatory)][bool] $EnableExtension
+    )
+    $Uri = "https://admin.microsoft.com/fd/bfb/api/v3/office/switch/feature"
+
+    if ($EnableExtension -eq $false) {
+        $Body = @{
+            features = @('AccountLinking')
+        }
+        $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method DELETE -Body $Body
+    } elseif ($EnableExtension -eq $true) {
+        $Body = @{
+            features = @('AccountLinking')
+        }
+        $null = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method PUT -Body $Body
+    }
+}
