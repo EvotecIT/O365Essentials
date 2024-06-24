55465a744f28d2079a7ab4471c550fb4a8951467
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:09:01 -0500

    Update Get-O365OrgCalendarSharing.ps1
    updated help

--- Public/Get-O365OrgCalendarSharing.ps1
+++ Public/Get-O365OrgCalendarSharing.ps1
@@ -1,19 +1,15 @@
 ﻿function Get-O365OrgCalendarSharing {
     <#
-    .SYNOPSIS
-    Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
-
-    .DESCRIPTION
-    Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
-
-    .PARAMETER Headers
-    Authentication Token along with additional information that is created with Connect-O365Admin. If heaaders are not provided it will use the default token.
-
-    .EXAMPLE
-    Get-O365CalendarSharing
-
-    .NOTES
-    General notes
+        .SYNOPSIS
+        Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
+        .DESCRIPTION
+        Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
+        .PARAMETER Headers
+        Authentication Token along with additional information that is created with Connect-O365Admin. If heaaders are not provided it will use the default token.
+        .EXAMPLE
+        Get-O365CalendarSharing
+        .NOTES
+        General notes
     #>
     [cmdletbinding()]
     param(
