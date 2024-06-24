da81a9a9b750d646c617df4838aa469321cc267f
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:25:49 -0500

    Update Set-O365OrgCustomerLockbox.ps1
    added help

--- Public/Set-O365OrgCustomerLockbox.ps1
+++ Public/Set-O365OrgCustomerLockbox.ps1
@@ -1,4 +1,22 @@
 ﻿function Set-O365OrgCustomerLockbox {
+    <#
+        .SYNOPSIS
+        Configures the Customer Lockbox settings for an Office 365 organization.
+        .DESCRIPTION
+        This function allows you to enable or disable the Customer Lockbox feature for your Office 365 organization.
+        It sends a POST request to the Office 365 admin API with the specified settings.
+        .PARAMETER Headers
+        Specifies the headers for the API request. Typically includes authorization tokens.
+        .PARAMETER RequireApproval
+        Specifies whether Customer Lockbox should require approval.
+        .EXAMPLE
+        $headers = @{Authorization = "Bearer your_token"}
+        Set-O365OrgCustomerLockbox -Headers $headers -RequireApproval $true
+
+        This example enables the Customer Lockbox feature for the Office 365 organization.
+        .NOTES
+        This function sends a POST request to the Office 365 admin API with the specified settings.
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
