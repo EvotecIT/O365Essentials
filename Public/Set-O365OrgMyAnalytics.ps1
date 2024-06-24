dc8c1a67661fd525d49dbd6b0a12267ea4506bb1
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:18:24 -0500

    Update Set-O365OrgMyAnalytics.ps1
    updated help formatting

--- Public/Set-O365OrgMyAnalytics.ps1
+++ Public/Set-O365OrgMyAnalytics.ps1
@@ -1,23 +1,29 @@
 ﻿function Set-O365OrgMyAnalytics {
     <#
-        .SYNOPSIS
-        Configures MyAnalytics settings for an Office 365 organization.
-        .DESCRIPTION
-        This function allows you to configure the MyAnalytics settings for your Office 365 organization. 
-        It sends a POST request to the Office 365 admin API with the specified settings.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER EnableInsightsDashboard
-        Specifies whether the Insights Dashboard should be enabled or disabled.
-        .PARAMETER EnableWeeklyDigest
-        Specifies whether the Weekly Digest emails should be enabled or disabled.
-        .PARAMETER EnableInsightsOutlookAddIn
-        Specifies whether the Insights Outlook Add-In should be enabled or disabled.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgMyAnalytics -Headers $headers -EnableInsightsDashboard $true -EnableWeeklyDigest $false -EnableInsightsOutlookAddIn $true
+    .SYNOPSIS
+    Configures MyAnalytics settings for an Office 365 organization.
 
-        This example enables the Insights Dashboard and the Insights Outlook Add-In, and disables the Weekly Digest emails for the Office 365 organization.
+    .DESCRIPTION
+    This function allows you to configure the MyAnalytics settings for your Office 365 organization. 
+    It sends a POST request to the Office 365 admin API with the specified settings.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER EnableInsightsDashboard
+    Specifies whether the Insights Dashboard should be enabled or disabled.
+
+    .PARAMETER EnableWeeklyDigest
+    Specifies whether the Weekly Digest emails should be enabled or disabled.
+
+    .PARAMETER EnableInsightsOutlookAddIn
+    Specifies whether the Insights Outlook Add-In should be enabled or disabled.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgMyAnalytics -Headers $headers -EnableInsightsDashboard $true -EnableWeeklyDigest $false -EnableInsightsOutlookAddIn $true
+
+    This example enables the Insights Dashboard and the Insights Outlook Add-In, and disables the Weekly Digest emails for the Office 365 organization.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
