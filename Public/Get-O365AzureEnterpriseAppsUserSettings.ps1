98c70151530933f595e0a35d9485df605da2d3b2
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:38:00 -0500

    Update Get-O365AzureEnterpriseAppsUserSettings.ps1
    added help

--- Public/Get-O365AzureEnterpriseAppsUserSettings.ps1
+++ Public/Get-O365AzureEnterpriseAppsUserSettings.ps1
@@ -1,12 +1,27 @@
 ﻿function Get-O365AzureEnterpriseAppsUserSettings {
-    # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
-    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
-    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/UserSettings/menuId/
+    <#
+        .SYNOPSIS
+        Retrieves user settings for Azure Enterprise Apps.
+        .DESCRIPTION
+        This function retrieves user settings for Azure Enterprise Apps based on the provided headers.
+        It returns information about user consent settings for accessing data, adding gallery apps, and visibility of Office 365 apps in the portal.
+        .PARAMETER Headers
+        A dictionary containing the headers for the API request, typically including authorization information.
+        .PARAMETER NoTranslation
+        Specifies whether to return the user settings without translation.
+        .EXAMPLE
+        Get-O365AzureEnterpriseAppsUserSettings -Headers $headers
+        .NOTES
+        For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
         [switch] $NoTranslation
     )
+    # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/UserSettings/menuId/
     $Uri = 'https://main.iam.ad.ext.azure.com/api/EnterpriseApplications/UserSettings'
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
     if ($Output) {
