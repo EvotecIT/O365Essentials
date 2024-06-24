fed03e949fa1d5299474ee86ccae5cb6188d508b
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:30:21 -0500

    Update Set-O365OrgAzureSpeechServices.ps1
    updated help formatting

--- Public/Set-O365OrgAzureSpeechServices.ps1
+++ Public/Set-O365OrgAzureSpeechServices.ps1
@@ -1,17 +1,22 @@
 ﻿function Set-O365OrgAzureSpeechServices {
     <#
-        .SYNOPSIS
-        Provides functionality to enable or disable the organization-wide language model for Azure Speech Services.
-        .DESCRIPTION
-        This function allows enabling or disabling the organization-wide language model for Azure Speech Services.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .PARAMETER AllowTheOrganizationWideLanguageModel
-        Specifies whether to enable or disable the organization-wide language model.
-        .EXAMPLE
-        Set-O365OrgAzureSpeechServices -Headers $headers -AllowTheOrganizationWideLanguageModel $true
-        .NOTES
-        For more information, visit: https://admin.microsoft.com/admin/api/services/apps/azurespeechservices
+    .SYNOPSIS
+    Provides functionality to enable or disable the organization-wide language model for Azure Speech Services.
+
+    .DESCRIPTION
+    This function allows enabling or disabling the organization-wide language model for Azure Speech Services.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .PARAMETER AllowTheOrganizationWideLanguageModel
+    Specifies whether to enable or disable the organization-wide language model.
+
+    .EXAMPLE
+    Set-O365OrgAzureSpeechServices -Headers $headers -AllowTheOrganizationWideLanguageModel $true
+
+    .NOTES
+    For more information, visit: https://admin.microsoft.com/admin/api/services/apps/azurespeechservices
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
