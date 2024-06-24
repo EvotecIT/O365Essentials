0f67caefff872e54f66e30aef13dea72dfd840ec
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:27:35 -0500

    Update Get-O365BillingAccounts.ps1
    added help

--- Public/Get-O365BillingAccounts.ps1
+++ Public/Get-O365BillingAccounts.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365BillingAccounts {
+    <#
+        .SYNOPSIS
+        Retrieves billing accounts information from Office 365.
+        .DESCRIPTION
+        This function retrieves billing accounts information from Office 365 using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365BillingAccounts -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
