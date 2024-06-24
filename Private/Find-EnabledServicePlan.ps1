572accad2093f713f8f9c52d92da1fb3592f3a7c
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:45:41 -0500

    Update Find-EnabledServicePlan.ps1
    added help

--- Private/Find-EnabledServicePlan.ps1
+++ Private/Find-EnabledServicePlan.ps1
@@ -1,4 +1,24 @@
 ﻿function Find-EnabledServicePlan {
+    <#
+        .SYNOPSIS
+        Identifies enabled and disabled service plans from a given list.
+        .DESCRIPTION
+        This function takes two arrays: one containing all service plans and another containing disabled service plans. 
+        It returns an ordered dictionary with two keys: 'Enabled' and 'Disabled'. The 'Enabled' key contains an array of service plans that are not in the disabled list, 
+        and the 'Disabled' key contains an array of service plans that are in the disabled list.
+        .PARAMETER ServicePlans
+        An array of all available service plans.
+        .PARAMETER DisabledServicePlans
+        An array of service plans that are disabled.
+        .EXAMPLE
+        $allPlans = @('PlanA', 'PlanB', 'PlanC')
+        $disabledPlans = @('PlanB')
+        $result = Find-EnabledServicePlan -ServicePlans $allPlans -DisabledServicePlans $disabledPlans
+        # $result.Enabled will contain 'PlanA' and 'PlanC'
+        # $result.Disabled will contain 'PlanB'
+        .NOTES
+        This function is useful for categorizing service plans into enabled and disabled groups.
+    #>
     [cmdletbinding()]
     param(
         [Array] $ServicePlans,
