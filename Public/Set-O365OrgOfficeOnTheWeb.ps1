bba47da3172bb098b0ad99364aa1d44e3aeccd38
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:17:15 -0500

    Update Set-O365OrgOfficeOnTheWeb.ps1
    updated help formatting

--- Public/Set-O365OrgOfficeOnTheWeb.ps1
+++ Public/Set-O365OrgOfficeOnTheWeb.ps1
@@ -1,23 +1,28 @@
 ﻿function Set-O365OrgOfficeOnTheWeb {
     <#
-        .SYNOPSIS
-        Enables or disables Office on the web for an Office 365 tenant.
-        .DESCRIPTION
-        This function allows you to enable or disable the Office on the web feature for your Office 365 organization. It sends a POST request to the Office 365 admin API with the specified settings.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER Enabled
-        Specifies whether Office on the web should be enabled or disabled. This parameter is mandatory.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgOfficeOnTheWeb -Headers $headers -Enabled $true
+    .SYNOPSIS
+    Enables or disables Office on the web for an Office 365 tenant.
 
-        This example enables Office on the web for the Office 365 organization.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgOfficeOnTheWeb -Headers $headers -Enabled $false
+    .DESCRIPTION
+    This function allows you to enable or disable the Office on the web feature for your Office 365 organization. It sends a POST request to the Office 365 admin API with the specified settings.
 
-        This example disables Office on the web for the Office 365 organization.
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER Enabled
+    Specifies whether Office on the web should be enabled or disabled. This parameter is mandatory.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgOfficeOnTheWeb -Headers $headers -Enabled $true
+
+    This example enables Office on the web for the Office 365 organization.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgOfficeOnTheWeb -Headers $headers -Enabled $false
+
+    This example disables Office on the web for the Office 365 organization.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
