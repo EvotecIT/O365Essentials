6225993d2e0b0f30f72fde238a48b543253697ad
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:10:47 -0500

    Update Get-O365OrgAzureSpeechServices.ps1
    added help

--- Public/Get-O365OrgAzureSpeechServices.ps1
+++ Public/Get-O365OrgAzureSpeechServices.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365OrgAzureSpeechServices {
+    <#
+        .SYNOPSIS
+        Retrieves the status of Azure Speech Services for the organization.
+        .DESCRIPTION
+        This function queries the Microsoft Graph API to retrieve the status of Azure Speech Services for the organization.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365OrgAzureSpeechServices -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [parameter()][alias('Authorization')][System.Collections.IDictionary] $Headers
