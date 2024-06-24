aa7c9fdf9522ba83a5f8b94f520d025221b5621f
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:32:04 -0500

    Update Get-O365OrgCustomerLockbox.ps1
    updated help formatting

--- Public/Get-O365OrgCustomerLockbox.ps1
+++ Public/Get-O365OrgCustomerLockbox.ps1
@@ -1,16 +1,20 @@
 ﻿function Get-O365OrgCustomerLockbox {
     <#
-        .SYNOPSIS
-        Retrieves customer lockbox information for the organization.
-        .DESCRIPTION
-        This function retrieves customer lockbox information for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        Specifies the headers containing the authorization information.
-        .EXAMPLE
-        Get-O365OrgCustomerLockbox -Headers $Headers
-        An example of how to retrieve customer lockbox information.
-        .NOTES
-        General notes
+    .SYNOPSIS
+    Retrieves customer lockbox information for the organization.
+
+    .DESCRIPTION
+    This function retrieves customer lockbox information for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    Specifies the headers containing the authorization information.
+
+    .EXAMPLE
+    Get-O365OrgCustomerLockbox -Headers $Headers
+    An example of how to retrieve customer lockbox information.
+
+    .NOTES
+    General notes
     #>
     [cmdletbinding()]
     param(
