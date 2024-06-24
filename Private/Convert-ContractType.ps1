fce0e0810965e16f8f9c3d08623be458e09e886f
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 10:49:57 -0500

    Update Convert-ContractType.ps1
    updated help formatting

--- Private/Convert-ContractType.ps1
+++ Private/Convert-ContractType.ps1
@@ -1,23 +1,23 @@
 ﻿function Convert-ContractType {
     <#
-        .SYNOPSIS
-        Converts contract type codes to their descriptive names.
+    .SYNOPSIS
+    Converts contract type codes to their descriptive names.
 
-        .DESCRIPTION
-        This function takes an array of contract type codes and converts each code to its corresponding descriptive name.
-        If a code does not have a corresponding name, the code itself is returned.
+    .DESCRIPTION
+    This function takes an array of contract type codes and converts each code to its corresponding descriptive name.
+    If a code does not have a corresponding name, the code itself is returned.
         
-        .PARAMETER ContractType
-        An array of contract type codes that need to be converted to descriptive names.
+    .PARAMETER ContractType
+    An array of contract type codes that need to be converted to descriptive names.
         
-        .EXAMPLE
-        Convert-ContractType -ContractType '3', '1'
-        # Returns 'Reseller', '1'
-        # Note: '1' is returned as is because it does not have a corresponding name in the mapping.
+    .EXAMPLE
+    Convert-ContractType -ContractType '3', '1'
+    # Returns 'Reseller', '1'
+    # Note: '1' is returned as is because it does not have a corresponding name in the mapping.
         
-        .NOTES
-        Current mappings include:
-        '3' for 'Reseller'
+    .NOTES
+    Current mappings include:
+    '3' for 'Reseller'
     #>
     [cmdletbinding()]
     param(
