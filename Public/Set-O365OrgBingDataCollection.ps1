23405a400ef47c421c3c1986488de274c6736f0c
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:29:50 -0500

    Update Set-O365OrgBingDataCollection.ps1
    updated help formatting

--- Public/Set-O365OrgBingDataCollection.ps1
+++ Public/Set-O365OrgBingDataCollection.ps1
@@ -1,17 +1,22 @@
 ﻿function Set-O365OrgBingDataCollection {
     <#
-        .SYNOPSIS
-        Provides functionality to set consent for Bing data collection in the organization.
-        .DESCRIPTION
-        This function allows setting consent for Bing data collection in the organization.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .PARAMETER IsBingDataCollectionConsented
-        Specifies whether Bing data collection is consented or not.
-        .EXAMPLE
-        Set-O365OrgBingDataCollection -Headers $headers -IsBingDataCollectionConsented $true
-        .NOTES
-        For more information, visit: https://admin.microsoft.com/admin/api/settings/security/bingdatacollection
+    .SYNOPSIS
+    Provides functionality to set consent for Bing data collection in the organization.
+
+    .DESCRIPTION
+    This function allows setting consent for Bing data collection in the organization.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .PARAMETER IsBingDataCollectionConsented
+    Specifies whether Bing data collection is consented or not.
+
+    .EXAMPLE
+    Set-O365OrgBingDataCollection -Headers $headers -IsBingDataCollectionConsented $true
+
+    .NOTES
+    For more information, visit: https://admin.microsoft.com/admin/api/settings/security/bingdatacollection
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
