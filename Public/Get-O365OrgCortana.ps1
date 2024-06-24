14f18883e26c6bc302d2c2de20122ad16752383e
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:32:59 -0500

    Update Get-O365OrgCortana.ps1
    updated help formatting

--- Public/Get-O365OrgCortana.ps1
+++ Public/Get-O365OrgCortana.ps1
@@ -1,16 +1,20 @@
 ﻿function Get-O365OrgCortana {
     <#
-        .SYNOPSIS
-        Retrieves Cortana app information for the organization.
-        .DESCRIPTION
-        This function retrieves Cortana app information for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        Specifies the headers containing the authorization information.
-        .EXAMPLE
-        Get-O365OrgCortana -Headers $Headers
-        An example of how to retrieve Cortana app information.
-        .NOTES
-        General notes
+    .SYNOPSIS
+    Retrieves Cortana app information for the organization.
+
+    .DESCRIPTION
+    This function retrieves Cortana app information for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    Specifies the headers containing the authorization information.
+
+    .EXAMPLE
+    Get-O365OrgCortana -Headers $Headers
+    An example of how to retrieve Cortana app information.
+
+    .NOTES
+    General notes
     #>
     [cmdletbinding()]
     param(
