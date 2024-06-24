020667fabf20872b7c0358e79fe7ad5699e40bc2
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:30:35 -0500

    Update Get-O365OrgDynamics365SalesInsights.ps1
    updated help formatting

--- Public/Get-O365OrgDynamics365SalesInsights.ps1
+++ Public/Get-O365OrgDynamics365SalesInsights.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-O365OrgDynamics365SalesInsights {
     <#
-        .SYNOPSIS
-        Retrieves Dynamics 365 Sales Insights information for the organization.
-        .DESCRIPTION
-        This function retrieves Dynamics 365 Sales Insights information for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365OrgDynamics365SalesInsights -Headers $headers
+    .SYNOPSIS
+    Retrieves Dynamics 365 Sales Insights information for the organization.
+
+    .DESCRIPTION
+    This function retrieves Dynamics 365 Sales Insights information for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365OrgDynamics365SalesInsights -Headers $headers
     #>
     [cmdletbinding()]
     param(
