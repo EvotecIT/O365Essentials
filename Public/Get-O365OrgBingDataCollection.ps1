d1818518f668db654c1163aab3adb771eb07c289
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:10:19 -0500

    Update Get-O365OrgBingDataCollection.ps1
    added help

--- Public/Get-O365OrgBingDataCollection.ps1
+++ Public/Get-O365OrgBingDataCollection.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365OrgBingDataCollection {
+    <#
+        .SYNOPSIS
+        Retrieves the Bing Data Collection settings for the organization.
+        .DESCRIPTION
+        This function queries the Microsoft Graph API to retrieve the Bing Data Collection settings for the organization.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365OrgBingDataCollection -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
