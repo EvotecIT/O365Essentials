7b62f2862e85347d6b052e604755b4ea9151c0fb
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:16:00 -0500

    Update Set-O365OrgPrivacyProfile.ps1
    added help

--- Public/Set-O365OrgPrivacyProfile.ps1
+++ Public/Set-O365OrgPrivacyProfile.ps1
@@ -1,4 +1,21 @@
 ﻿function Set-O365OrgPrivacyProfile {
+    <#
+        .SYNOPSIS
+        Configures the privacy profile settings for an Office 365 organization.
+        .DESCRIPTION
+        This function updates the privacy profile settings for an Office 365 organization. It allows specifying the privacy statement URL and the privacy contact information.
+        .PARAMETER Headers
+        Specifies the headers for the API request. Typically includes authorization tokens.
+        .PARAMETER PrivacyUrl
+        Specifies the URL of the privacy statement. Accepts a URI value.
+        .PARAMETER PrivacyContact
+        Specifies the contact information for privacy-related inquiries. Accepts a string value.
+        .EXAMPLE
+        $headers = @{Authorization = "Bearer your_token"}
+        Set-O365OrgPrivacyProfile -Headers $headers -PrivacyUrl "https://example.com/privacy" -PrivacyContact "privacy@example.com"
+
+        This example sets the privacy statement URL to "https://example.com/privacy" and the privacy contact to "privacy@example.com".
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
