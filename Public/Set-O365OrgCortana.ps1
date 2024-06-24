aed26d4833c19eeec72ee601e0c590e520ff19eb
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:26:20 -0500

    Update Set-O365OrgCortana.ps1
    updated help

--- Public/Set-O365OrgCortana.ps1
+++ Public/Set-O365OrgCortana.ps1
@@ -1,22 +1,21 @@
 ﻿function Set-O365OrgCortana {
     <#
-    .SYNOPSIS
-    Short description
+        .SYNOPSIS
+        Configures the Cortana settings for an Office 365 organization.
+        .DESCRIPTION
+        This function allows you to enable or disable the Cortana service for your Office 365 organization.
+        It sends a POST request to the Office 365 admin API with the specified settings.
+        .PARAMETER Headers
+        Specifies the headers for the API request. Typically includes authorization tokens.
+        .PARAMETER Enabled
+        Specifies whether the Cortana service should be enabled.
+        .EXAMPLE
+        $headers = @{Authorization = "Bearer your_token"}
+        Set-O365OrgCortana -Headers $headers -Enabled $true
 
-    .DESCRIPTION
-    Long description
-
-    .PARAMETER Headers
-    Parameter description
-
-    .PARAMETER Enabled
-    Parameter description
-
-    .EXAMPLE
-    An example
-
-    .NOTES
-    General notes
+        This example enables the Cortana service for the Office 365 organization.
+        .NOTES
+        This function sends a POST request to the Office 365 admin API with the specified settings.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
