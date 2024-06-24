f459a611b0781266ac36b7305b3e4fc8bba7ec41
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:58:47 -0500

    Update Get-O365OrgOfficeOnTheWeb.ps1
    added help

--- Public/Get-O365OrgOfficeOnTheWeb.ps1
+++ Public/Get-O365OrgOfficeOnTheWeb.ps1
@@ -1,4 +1,16 @@
 ﻿function Get-O365OrgOfficeOnTheWeb {
+    <#
+        .SYNOPSIS
+        Retrieves settings for Office Online apps in the organization.
+        .DESCRIPTION
+        This function retrieves settings for Office Online apps in the organization from the specified URI.
+        .PARAMETER Headers
+        Authentication token and additional information created with Connect-O365Admin.
+        .EXAMPLE
+        Get-O365OrgOfficeOnTheWeb -Headers $headers
+        .NOTES
+        This function retrieves settings for Office Online apps from the specified URI.
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
