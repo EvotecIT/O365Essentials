f75957c300450c2a08a6c1ef1b9ffe70c1077472
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:07:27 -0500

    Update Get-O365OrgCortana.ps1
    added help

--- Public/Get-O365OrgCortana.ps1
+++ Public/Get-O365OrgCortana.ps1
@@ -1,4 +1,17 @@
 ﻿function Get-O365OrgCortana {
+    <#
+        .SYNOPSIS
+        Retrieves Cortana app information for the organization.
+        .DESCRIPTION
+        This function retrieves Cortana app information for the organization from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        Specifies the headers containing the authorization information.
+        .EXAMPLE
+        Get-O365OrgCortana -Headers $Headers
+        An example of how to retrieve Cortana app information.
+        .NOTES
+        General notes
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
