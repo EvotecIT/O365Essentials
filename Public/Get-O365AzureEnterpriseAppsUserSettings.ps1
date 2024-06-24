222ca7fdcb563e9120949c253f3abd8afdababfe
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:50:45 -0500

    Update Get-O365AzureEnterpriseAppsUserSettings.ps1
    updated help formatting

--- Public/Get-O365AzureEnterpriseAppsUserSettings.ps1
+++ Public/Get-O365AzureEnterpriseAppsUserSettings.ps1
@@ -1,18 +1,23 @@
 ﻿function Get-O365AzureEnterpriseAppsUserSettings {
     <#
-        .SYNOPSIS
-        Retrieves user settings for Azure Enterprise Apps.
-        .DESCRIPTION
-        This function retrieves user settings for Azure Enterprise Apps based on the provided headers.
-        It returns information about user consent settings for accessing data, adding gallery apps, and visibility of Office 365 apps in the portal.
-        .PARAMETER Headers
-        A dictionary containing the headers for the API request, typically including authorization information.
-        .PARAMETER NoTranslation
-        Specifies whether to return the user settings without translation.
-        .EXAMPLE
-        Get-O365AzureEnterpriseAppsUserSettings -Headers $headers
-        .NOTES
-        For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    .SYNOPSIS
+    Retrieves user settings for Azure Enterprise Apps.
+
+    .DESCRIPTION
+    This function retrieves user settings for Azure Enterprise Apps based on the provided headers.
+    It returns information about user consent settings for accessing data, adding gallery apps, and visibility of Office 365 apps in the portal.
+
+    .PARAMETER Headers
+    A dictionary containing the headers for the API request, typically including authorization information.
+
+    .PARAMETER NoTranslation
+    Specifies whether to return the user settings without translation.
+
+    .EXAMPLE
+    Get-O365AzureEnterpriseAppsUserSettings -Headers $headers
+
+    .NOTES
+    For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
     #>
     [cmdletbinding()]
     param(
