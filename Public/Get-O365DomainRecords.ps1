26b89c9cee3c8c3a26f4672a5742f3b24bec1d50
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:38:22 -0500

    Update Get-O365DomainRecords.ps1
    updated help formatting

--- Public/Get-O365DomainRecords.ps1
+++ Public/Get-O365DomainRecords.ps1
@@ -1,15 +1,19 @@
 ﻿function Get-O365DomainRecords {
     <#
-        .SYNOPSIS
-        Provides functionality to retrieve domain records for a specified domain in Office 365.
-        .DESCRIPTION
-        This function allows you to query and retrieve domain records for a specific domain in Office 365 using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .PARAMETER DomainName
-        The name of the domain for which to retrieve records.
-        .EXAMPLE
-        Get-O365DomainRecords -Headers $headers -DomainName 'example.com'
+    .SYNOPSIS
+    Provides functionality to retrieve domain records for a specified domain in Office 365.
+
+    .DESCRIPTION
+    This function allows you to query and retrieve domain records for a specific domain in Office 365 using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .PARAMETER DomainName
+    The name of the domain for which to retrieve records.
+
+    .EXAMPLE
+    Get-O365DomainRecords -Headers $headers -DomainName 'example.com'
     #>
     [cmdletbinding()]
     param(
