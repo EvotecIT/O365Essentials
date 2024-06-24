3beb9e8039209a888627901b536ed5b6133ed125
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:30:15 -0500

    Update Get-O365OrgForms.ps1
    updated help formatting

--- Public/Get-O365OrgForms.ps1
+++ Public/Get-O365OrgForms.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-O365OrgForms {
     <#
-        .SYNOPSIS
-        Retrieves information about Office Forms for the organization.
-        .DESCRIPTION
-        This function retrieves information about Office Forms for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365OrgForms -Headers $headers
+    .SYNOPSIS
+    Retrieves information about Office Forms for the organization.
+
+    .DESCRIPTION
+    This function retrieves information about Office Forms for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365OrgForms -Headers $headers
     #>
     [cmdletbinding()]
     param(
