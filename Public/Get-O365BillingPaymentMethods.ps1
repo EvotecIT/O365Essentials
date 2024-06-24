dfbce473058583a222a0e60594941d23386a1739
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:24:11 -0500

    Update Get-O365BillingPaymentMethods.ps1
    added help

--- Public/Get-O365BillingPaymentMethods.ps1
+++ Public/Get-O365BillingPaymentMethods.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365BillingPaymentMethods {
+    <#
+        .SYNOPSIS
+        Retrieves unsettled charges for payment instruments in the specified organization.
+        .DESCRIPTION
+        This function retrieves unsettled charges for payment instruments in the organization from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365BillingPaymentMethods -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
