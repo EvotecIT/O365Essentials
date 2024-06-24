7f32f7593a3f2cafefb0879ea964fa087762e5a6
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:35:48 -0500

    Update Get-O365AzureExternalCollaborationSettings.ps1
    added help

--- Public/Get-O365AzureExternalCollaborationSettings.ps1
+++ Public/Get-O365AzureExternalCollaborationSettings.ps1
@@ -1,4 +1,12 @@
 ﻿function Get-O365AzureExternalCollaborationSettings {
+    <#
+        .SYNOPSIS
+        Retrieves Azure external collaboration settings based on the provided headers.
+        .DESCRIPTION
+        This function retrieves Azure external collaboration settings from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
