4b7d4350e4ea816c95c9794ba8e0838f271fa80e
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:43:18 -0500

    Update Get-O365BillingLicenseRequests.ps1
    updated help formatting

--- Public/Get-O365BillingLicenseRequests.ps1
+++ Public/Get-O365BillingLicenseRequests.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-O365BillingLicenseRequests {
     <#
-        .SYNOPSIS
-        Retrieves self-service license requests for a specific Office 365 tenant.
-        .DESCRIPTION
-        This function retrieves self-service license requests for a specific Office 365 tenant using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365BillingLicenseRequests -Headers $headers
+    .SYNOPSIS
+    Retrieves self-service license requests for a specific Office 365 tenant.
+
+    .DESCRIPTION
+    This function retrieves self-service license requests for a specific Office 365 tenant using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365BillingLicenseRequests -Headers $headers
     #>
     [cmdletbinding()]
     param(
