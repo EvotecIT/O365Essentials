e1d3d68848f25a0313bad23832cd8423b890db27
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:27:20 -0500

    Update Set-O365OrgCalendarSharing.ps1
    updated help formatting

--- Public/Set-O365OrgCalendarSharing.ps1
+++ Public/Set-O365OrgCalendarSharing.ps1
@@ -1,24 +1,31 @@
 ﻿function Set-O365OrgCalendarSharing {
     <#
-        .SYNOPSIS
-        Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
-        .DESCRIPTION
-        Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
-        .PARAMETER Headers
-        Authentication Token along with additional information that is created with Connect-O365Admin. If heaaders are not provided it will use the default token.
-        .PARAMETER EnableAnonymousCalendarSharing
-        Enables or Disables anonymous calendar sharing
-        .PARAMETER EnableCalendarSharing
-        Enables or Disables calendar sharing
-        .PARAMETER SharingOption
-        Decide on how to share the calendar
-            - Show calendar free/busy information with time only (CalendarSharingFreeBusySimple)
-            - Show calendar free/busy information with time, subject and location (CalendarSharingFreeBusyDetail)
-            - Show all calendar appointment information (CalendarSharingFreeBusyReviewer)
-        .EXAMPLE
-        Set-O365CalendarSharing -EnableCalendarSharing $false
-        .NOTES
-        General notes
+    .SYNOPSIS
+    Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
+
+    .DESCRIPTION
+    Let your users share their calendars with people outside of your organization who have Office 365 or Exchange
+
+    .PARAMETER Headers
+    Authentication Token along with additional information that is created with Connect-O365Admin. If heaaders are not provided it will use the default token.
+
+    .PARAMETER EnableAnonymousCalendarSharing
+    Enables or Disables anonymous calendar sharing
+
+    .PARAMETER EnableCalendarSharing
+    Enables or Disables calendar sharing
+
+    .PARAMETER SharingOption
+    Decide on how to share the calendar
+    - Show calendar free/busy information with time only (CalendarSharingFreeBusySimple)
+    - Show calendar free/busy information with time, subject and location (CalendarSharingFreeBusyDetail)
+    - Show all calendar appointment information (CalendarSharingFreeBusyReviewer)
+
+    .EXAMPLE
+    Set-O365CalendarSharing -EnableCalendarSharing $false
+
+    .NOTES
+    General notes
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
