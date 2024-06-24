55cf9722b09c2d8cff0e13b0f7beaddaf44e4921
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 10:21:45 -0500

    Update Get-O365PrivateUserOrSPN.ps1
    updated help formatting

--- Private/Get-O365PrivateUserOrSPN.ps1
+++ Private/Get-O365PrivateUserOrSPN.ps1
@@ -2,15 +2,19 @@
     <#
         .SYNOPSIS
         Retrieves an Office 365 user or service principal by their principal ID.
+
         .DESCRIPTION
         This function attempts to retrieve an Office 365 user or service principal using the provided principal ID. 
         It first tries to find a user with the given ID. If no user is found, it then tries to find a service principal with the same ID.
         If neither a user nor a service principal is found, it outputs any warnings encountered during the process.
+        
         .PARAMETER PrincipalID
         The ID of the principal (user or service principal) to retrieve.
+        
         .EXAMPLE
         $principal = Get-O365PrivateUserOrSPN -PrincipalID "user@example.com"
         This example retrieves the Office 365 user or service principal with the ID "user@example.com".
+        
         .NOTES
         This function is useful for identifying whether a given principal ID corresponds to a user or a service principal in Office 365.
     #>
