9825c3191e53a2c6d1dcd0647eccda540ff07d38
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:16:09 -0500

    Update Get-O365User.ps1
    updated help formatting

--- Public/Get-O365User.ps1
+++ Public/Get-O365User.ps1
@@ -1,29 +1,40 @@
 ﻿function Get-O365User {
     <#
-        .SYNOPSIS
-        Provides functionality to retrieve Office 365 user information based on various parameters.
-        .DESCRIPTION
-        This function allows you to query and retrieve user information from Office 365 based on different criteria such as UserPrincipalName, EmailAddress, and ID.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .PARAMETER Id
-        The ID of the user to query.
-        .PARAMETER UserPrincipalName
-        The UserPrincipalName of the user to query.
-        .PARAMETER EmailAddress
-        The email address of the user to query.
-        .PARAMETER Property
-        An array of properties to include in the query response.
-        .PARAMETER Filter
-        The filter to apply to the query.
-        .PARAMETER GuestsOnly
-        Switch parameter to query only guest users.
-        .PARAMETER OrderBy
-        The property to order the query results by.
-        .EXAMPLE
-        Get-O365User -Headers $headers -UserPrincipalName 'john.doe@example.com' -Property @('displayName', 'jobTitle')
-        .NOTES
-        For more information, visit: https://docs.microsoft.com/en-us/graph/api/user-get?view=graph-rest-1.0
+    .SYNOPSIS
+    Provides functionality to retrieve Office 365 user information based on various parameters.
+
+    .DESCRIPTION
+    This function allows you to query and retrieve user information from Office 365 based on different criteria such as UserPrincipalName, EmailAddress, and ID.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .PARAMETER Id
+    The ID of the user to query.
+
+    .PARAMETER UserPrincipalName
+    The UserPrincipalName of the user to query.
+
+    .PARAMETER EmailAddress
+    The email address of the user to query.
+
+    .PARAMETER Property
+    An array of properties to include in the query response.
+
+    .PARAMETER Filter
+    The filter to apply to the query.
+
+    .PARAMETER GuestsOnly
+    Switch parameter to query only guest users.
+
+    .PARAMETER OrderBy
+    The property to order the query results by.
+
+    .EXAMPLE
+    Get-O365User -Headers $headers -UserPrincipalName 'john.doe@example.com' -Property @('displayName', 'jobTitle')
+
+    .NOTES
+    For more information, visit: https://docs.microsoft.com/en-us/graph/api/user-get?view=graph-rest-1.0
     #>
     [cmdletBinding(DefaultParameterSetName = "Default")]
     param(
