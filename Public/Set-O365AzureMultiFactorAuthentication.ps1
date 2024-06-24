39aec5996828b52208bf009682b3256f144ce481
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:32:25 -0500

    Update Set-O365AzureMultiFactorAuthentication.ps1
    updated help

--- Public/Set-O365AzureMultiFactorAuthentication.ps1
+++ Public/Set-O365AzureMultiFactorAuthentication.ps1
@@ -1,80 +1,55 @@
 ﻿function Set-O365AzureMultiFactorAuthentication {
     <#
-    .SYNOPSIS
-    Short description
-
-    .DESCRIPTION
-    Long description
-
-    .PARAMETER Headers
-    Parameter description
-
-    .PARAMETER AccountLockoutDurationMinutes
-    Minutes until account is automatically unblocked
-
-    .PARAMETER AccountLockoutResetMinutes
-    Minutes until account lockout counter is reset
-
-    .PARAMETER AccountLockoutThreshold
-    Number of MFA denials to trigger account lockout
-
-    .PARAMETER AllowPhoneMenu
-    Parameter description
-
-    .PARAMETER BlockForFraud
-    Automatically block users who report fraud
-
-    .PARAMETER CallerId
-    MFA caller ID number (US phone number only)
-
-    .PARAMETER DefaultBypassTimespan
-    Default one-time bypass seconds
-
-    .PARAMETER EnableFraudAlert
-    Allow users to submit fraud alerts
-
-    .PARAMETER FraudCode
-    Code to report fraud during initial greeting
-
-    .PARAMETER FraudNotificationEmailAddresses
-    Recipient's Email Address
-
-    .PARAMETER OneTimeBypassEmailAddresses
-    Parameter description
-
-    .PARAMETER PinAttempts
-    Number of PIN attempts allowed per call
-
-    .PARAMETER SayExtensionDigits
-    Parameter description
-
-    .PARAMETER SmsTimeoutSeconds
-    Two-way text message timeout seconds
-
-    .PARAMETER Caches
-    Parameter description
-
-    .PARAMETER Notifications
-    Parameter description
-
-    .PARAMETER NotificationEmailAddresses
-    Parameter description
-
-    .PARAMETER Greetings
-    Parameter description
-
-    .PARAMETER BlockedUsers
-    Parameter description
-
-    .PARAMETER BypassedUsers
-    Parameter description
-
-    .EXAMPLE
-    An example
-
-    .NOTES
-    Based on: https://portal.azure.com/#blade/Microsoft_AAD_IAM/MultifactorAuthenticationMenuBlade/GettingStarted/fromProviders/
-
+        .SYNOPSIS
+        Configures Multi-Factor Authentication (MFA) settings for an Office 365 tenant.
+        .DESCRIPTION
+        This function allows administrators to modify various settings related to Multi-Factor Authentication (MFA) for their Office 365 tenant. It includes options such as account lockout policies, fraud alert configurations, and bypass settings.
+        .PARAMETER Headers
+        Specifies the headers for the API request, typically including authorization details.
+        .PARAMETER AccountLockoutDurationMinutes
+        Specifies the duration in minutes that an account remains locked after reaching the threshold of failed MFA attempts.
+        .PARAMETER AccountLockoutResetMinutes
+        Defines the time period in minutes after which the count of failed MFA attempts is reset.
+        .PARAMETER AccountLockoutThreshold
+        Number of MFA denials to trigger account lockout
+        .PARAMETER AllowPhoneMenu
+        Parameter description
+        .PARAMETER BlockForFraud
+        Automatically block users who report fraud
+        .PARAMETER CallerId
+        MFA caller ID number (US phone number only)
+        .PARAMETER DefaultBypassTimespan
+        Default one-time bypass seconds
+        .PARAMETER EnableFraudAlert
+        Allow users to submit fraud alerts
+        .PARAMETER FraudCode
+        Code to report fraud during initial greeting
+        .PARAMETER FraudNotificationEmailAddresses
+        Recipient's Email Address
+        .PARAMETER OneTimeBypassEmailAddresses
+        Recipient's One-Time Email Addresses for Bypass
+        .PARAMETER PinAttempts
+        Number of PIN attempts allowed per call
+        .PARAMETER SayExtensionDigits
+        Parameter description
+        .PARAMETER SmsTimeoutSeconds
+        Two-way text message timeout seconds
+        .PARAMETER Caches
+        Parameter description
+        .PARAMETER Notifications
+        Parameter description
+        .PARAMETER NotificationEmailAddresses
+        Parameter description
+        .PARAMETER Greetings
+        Parameter description
+        .PARAMETER BlockedUsers
+        Parameter description
+        .PARAMETER BypassedUsers
+        Parameter description
+        .EXAMPLE
+        An example
+        .NOTES
+        Based on: https://portal.azure.com/#blade/Microsoft_AAD_IAM/MultifactorAuthenticationMenuBlade/GettingStarted/fromProviders/
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
