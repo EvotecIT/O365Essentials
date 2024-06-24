0473cc5a949191363bc690defd019764ebe2ac10
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:54:59 -0500

    Update Get-O365OrgReports.ps1
    added help

--- Public/Get-O365OrgReports.ps1
+++ Public/Get-O365OrgReports.ps1
@@ -1,4 +1,16 @@
 ﻿function Get-O365OrgReports {
+    <#
+        .SYNOPSIS
+        Retrieves organization reports configuration.
+        .DESCRIPTION
+        This function retrieves the organization's reports configuration from the specified URI using the provided headers.
+        .PARAMETER Headers
+        Authentication token and additional information for the API request.
+        .EXAMPLE
+        Get-O365OrgReports -Headers $headers
+        .NOTES
+        This function retrieves organization reports configuration from the specified URI.
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
