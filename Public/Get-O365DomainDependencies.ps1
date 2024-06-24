7bfef2fd976e09f381f9f623d08cf4ddefc90041
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:15:28 -0500

    Update Get-O365DomainDependencies.ps1
    added help

--- Public/Get-O365DomainDependencies.ps1
+++ Public/Get-O365DomainDependencies.ps1
@@ -1,4 +1,18 @@
 ﻿function Get-O365DomainDependencies {
+    <#
+        .SYNOPSIS
+        Provides functionality to retrieve domain dependencies in Office 365.
+        .DESCRIPTION
+        This function allows you to query and retrieve dependencies related to a specific domain in Office 365 using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .PARAMETER DomainName
+        The name of the domain for which to retrieve dependencies.
+        .PARAMETER Type
+        Specifies the type of dependencies to retrieve. Valid values are 'All', 'Users', 'TeamsAndGroups', and 'Apps'. Default is 'All'.
+        .EXAMPLE
+        Get-O365DomainDependencies -Headers $headers -DomainName 'example.com' -Type 'Users'
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
