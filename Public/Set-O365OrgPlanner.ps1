15188215dde98c3a8f0fe9f11770fb2d2c2b7efe
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 11:51:54 -0500

    Update Set-O365OrgPlanner.ps1
    updated help formatting

--- Public/Set-O365OrgPlanner.ps1
+++ Public/Set-O365OrgPlanner.ps1
@@ -1,18 +1,22 @@
 ﻿function Set-O365OrgPlanner {
     <#
-        .SYNOPSIS
-        Configures the Planner settings for an Office 365 organization.
-        .DESCRIPTION
-        This function updates the Planner settings for an Office 365 organization. It allows enabling or disabling calendar sharing within Planner.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER AllowCalendarSharing
-        Specifies whether calendar sharing should be allowed in Planner. Accepts a boolean value.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgPlanner -Headers $headers -AllowCalendarSharing $true
+    .SYNOPSIS
+    Configures the Planner settings for an Office 365 organization.
 
-        This example enables calendar sharing in Planner.
+    .DESCRIPTION
+    This function updates the Planner settings for an Office 365 organization. It allows enabling or disabling calendar sharing within Planner.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER AllowCalendarSharing
+    Specifies whether calendar sharing should be allowed in Planner. Accepts a boolean value.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgPlanner -Headers $headers -AllowCalendarSharing $true
+
+    This example enables calendar sharing in Planner.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
