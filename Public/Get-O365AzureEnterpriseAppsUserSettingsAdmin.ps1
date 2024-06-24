17a0c0e4f5f977787fb71a3e8c674b7014ecb7ef
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:37:28 -0500

    Update Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
    added help

--- Public/Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
+++ Public/Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
@@ -1,12 +1,27 @@
 ﻿function Get-O365AzureEnterpriseAppsUserSettingsAdmin {
-    # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
-    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
-    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/UserSettings/menuId/
+    <#
+        .SYNOPSIS
+        Retrieves user settings for Azure Enterprise Apps with admin consent flow.
+        .DESCRIPTION
+        This function retrieves user settings for Azure Enterprise Apps with admin consent flow based on the provided headers.
+        It returns information about request expiration days, notifications, reminders, approvers, and approversV2.
+        .PARAMETER Headers
+        A dictionary containing the headers for the API request, typically including authorization information.
+        .PARAMETER NoTranslation
+        Specifies whether to return the user settings without translation.
+        .EXAMPLE
+        Get-O365AzureEnterpriseAppsUserSettingsAdmin -Headers $headers
+        .NOTES
+        For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
         [switch] $NoTranslation
     )
+    # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
+    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/StartboardApplicationsMenuBlade/UserSettings/menuId/
     $Uri = 'https://main.iam.ad.ext.azure.com/api/RequestApprovals/V2/PolicyTemplates?type=AdminConsentFlow'
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
     if ($Output) {
