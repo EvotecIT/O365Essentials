462c8dd4be3f9f0432ee9e74f13aedc6486bec15
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:24:05 -0500

    Update Get-O365OrgPrivilgedAccess.ps1
    updated help formatting

--- Public/Get-O365OrgPrivilgedAccess.ps1
+++ Public/Get-O365OrgPrivilgedAccess.ps1
@@ -1,15 +1,19 @@
 ﻿function Get-O365OrgPrivilegedAccess {
     <#
-        .SYNOPSIS
-        Retrieves information about privileged access settings for the organization.
-        .DESCRIPTION
-        This function retrieves information about privileged access settings from the specified URI using the provided headers.
-        .PARAMETER Headers
-        Authentication token and additional information for the API request.
-        .EXAMPLE
-        Get-O365OrgPrivilegedAccess -Headers $headers
-        .NOTES
-        This function retrieves information about privileged access settings from the specified URI.
+    .SYNOPSIS
+    Retrieves information about privileged access settings for the organization.
+
+    .DESCRIPTION
+    This function retrieves information about privileged access settings from the specified URI using the provided headers.
+
+    .PARAMETER Headers
+    Authentication token and additional information for the API request.
+
+    .EXAMPLE
+    Get-O365OrgPrivilegedAccess -Headers $headers
+
+    .NOTES
+    This function retrieves information about privileged access settings from the specified URI.
     #>
     [cmdletbinding()]
     param(
