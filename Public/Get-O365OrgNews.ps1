6ded526165ab8e8fbed8385c3dd8ee5101dce84a
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:59:11 -0500

    Update Get-O365OrgNews.ps1
    added help

--- Public/Get-O365OrgNews.ps1
+++ Public/Get-O365OrgNews.ps1
@@ -1,4 +1,18 @@
 ﻿function Get-O365OrgNews {
+    <#
+        .SYNOPSIS
+        Retrieves news options for Bing in the organization.
+        .DESCRIPTION
+        This function retrieves news options for Bing in the organization. It can return the content enabled on a new tab and whether company information and industry are enabled.
+        .PARAMETER Headers
+        Authentication token and additional information created with Connect-O365Admin.
+        .PARAMETER NoTranslation
+        Indicates whether to skip translation of news options.
+        .EXAMPLE
+        Get-O365OrgNews -Headers $headers -NoTranslation
+        .NOTES
+        This function retrieves news options for Bing from the specified URI.
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
