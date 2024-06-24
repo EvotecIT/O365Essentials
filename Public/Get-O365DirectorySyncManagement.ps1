f4cb4b8a1fa3ed4b6c76e426ec1bdb3648cb40f6
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:39:54 -0500

    Update Get-O365DirectorySyncManagement.ps1
    updated help formatting

--- Public/Get-O365DirectorySyncManagement.ps1
+++ Public/Get-O365DirectorySyncManagement.ps1
@@ -1,11 +1,13 @@
 ﻿function Get-O365DirectorySyncManagement {
     <#
-        .SYNOPSIS
-        Retrieves directory synchronization management details from Office 365.
-        .DESCRIPTION
-        This function retrieves directory synchronization management details from Office 365 using the specified API endpoint and headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
+    .SYNOPSIS
+    Retrieves directory synchronization management details from Office 365.
+
+    .DESCRIPTION
+    This function retrieves directory synchronization management details from Office 365 using the specified API endpoint and headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
     #>
     [cmdletbinding()]
     param(
