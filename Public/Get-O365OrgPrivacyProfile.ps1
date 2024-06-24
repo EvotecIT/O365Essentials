0ccaa11f39f4a7b800cddfe05db0470e851118fa
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:24:31 -0500

    Update Get-O365OrgPrivacyProfile.ps1
    updated help formatting

--- Public/Get-O365OrgPrivacyProfile.ps1
+++ Public/Get-O365OrgPrivacyProfile.ps1
@@ -1,15 +1,19 @@
 ﻿function Get-O365OrgPrivacyProfile {
     <#
-        .SYNOPSIS
-        Retrieves information about the organization's privacy policy.
-        .DESCRIPTION
-        This function retrieves information about the organization's privacy policy from the specified URI using the provided headers.
-        .PARAMETER Headers
-        Authentication token and additional information for the API request.
-        .EXAMPLE
-        Get-O365OrgPrivacyProfile -Headers $headers
-        .NOTES
-        This function retrieves information about the organization's privacy policy from the specified URI.
+    .SYNOPSIS
+    Retrieves information about the organization's privacy policy.
+
+    .DESCRIPTION
+    This function retrieves information about the organization's privacy policy from the specified URI using the provided headers.
+
+    .PARAMETER Headers
+    Authentication token and additional information for the API request.
+
+    .EXAMPLE
+    Get-O365OrgPrivacyProfile -Headers $headers
+
+    .NOTES
+    This function retrieves information about the organization's privacy policy from the specified URI.
     #>
     [cmdletbinding()]
     param(
