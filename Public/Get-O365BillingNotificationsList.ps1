882bbb285e45d70618e7673b7af467c41777b4d4
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:42:35 -0500

    Update Get-O365BillingNotificationsList.ps1
    updated help formatting

--- Public/Get-O365BillingNotificationsList.ps1
+++ Public/Get-O365BillingNotificationsList.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-O365BillingNotificationsList {
     <#
-        .SYNOPSIS
-        Retrieves a list of billing notification users in Office 365.
-        .DESCRIPTION
-        This function retrieves a list of billing notification users in Office 365 from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365BillingNotificationsList -Headers $headers
+    .SYNOPSIS
+    Retrieves a list of billing notification users in Office 365.
+
+    .DESCRIPTION
+    This function retrieves a list of billing notification users in Office 365 from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365BillingNotificationsList -Headers $headers
     #>
     [cmdletbinding()]
     param(
