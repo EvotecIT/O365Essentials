e3a37cedef5462ab92af9cb3a541afa92c8bc9f8
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:49:12 -0500

    Update Get-O365AzureExternalCollaborationFlows.ps1
    updated help formatting

--- Public/Get-O365AzureExternalCollaborationFlows.ps1
+++ Public/Get-O365AzureExternalCollaborationFlows.ps1
@@ -1,17 +1,21 @@
 ﻿function Get-O365AzureExternalCollaborationFlows {
     <#
-        .SYNOPSIS
-        Provides information about Azure external collaboration flows.
-        .DESCRIPTION
-        This function retrieves details about Azure external collaboration flows based on the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365AzureExternalCollaborationFlows -Headers $headers
-        .NOTES
-        WARNING: Invoke-O365Admin - Error JSON: Response status code does not indicate success:
-        403 (Forbidden). The application does not have any of the required delegated permissions
-        (Policy.Read.All, Policy.ReadWrite.AuthenticationFlows) to access the resource.
+    .SYNOPSIS
+    Provides information about Azure external collaboration flows.
+
+    .DESCRIPTION
+    This function retrieves details about Azure external collaboration flows based on the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365AzureExternalCollaborationFlows -Headers $headers
+
+    .NOTES
+    WARNING: Invoke-O365Admin - Error JSON: Response status code does not indicate success:
+    403 (Forbidden). The application does not have any of the required delegated permissions
+    (Policy.Read.All, Policy.ReadWrite.AuthenticationFlows) to access the resource.
     #>
     [cmdletbinding()]
     param(
