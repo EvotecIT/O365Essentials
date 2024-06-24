15bb47b9b5ba088e9fd92377a45085be50f10a30
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 10:50:22 -0500

    Update Convert-SKUToLicense.ps1
    updated help formatting

--- Private/Convert-SKUToLicense.ps1
+++ Private/Convert-SKUToLicense.ps1
@@ -1,20 +1,20 @@
 ﻿function Convert-SKUToLicense {
     <#
-        .SYNOPSIS
-        Converts a SKU to its corresponding license details.
+    .SYNOPSIS
+    Converts a SKU to its corresponding license details.
 
-        .DESCRIPTION
-        This function takes a SKU (Stock Keeping Unit) identifier and retrieves the associated service plans and license details.
+    .DESCRIPTION
+    This function takes a SKU (Stock Keeping Unit) identifier and retrieves the associated service plans and license details.
         
-        .PARAMETER SKU
-        The SKU identifier for which the license details need to be retrieved.
+    .PARAMETER SKU
+    The SKU identifier for which the license details need to be retrieved.
         
-        .EXAMPLE
-        Convert-SKUToLicense -SKU "ENTERPRISEPACK"
-        # Returns the service plans and license details for the specified SKU.
+    .EXAMPLE
+    Convert-SKUToLicense -SKU "ENTERPRISEPACK"
+    # Returns the service plans and license details for the specified SKU.
         
-        .NOTES
-        This function relies on the Get-O365AzureLicenses cmdlet to fetch the license details.
+    .NOTES
+    This function relies on the Get-O365AzureLicenses cmdlet to fetch the license details.
     #>
     [cmdletbinding()]
     param(
