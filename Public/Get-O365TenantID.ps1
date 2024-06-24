489e1b04a2955d8847e39e10a799928c075f6019
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:16:33 -0500

    Update Get-O365TenantID.ps1
    updated help formatting

--- Public/Get-O365TenantID.ps1
+++ Public/Get-O365TenantID.ps1
@@ -1,15 +1,19 @@
 ﻿function Get-O365TenantID {
     <#
-        .SYNOPSIS
-        Provides the tenant ID for a given domain.
-        .DESCRIPTION
-        This function retrieves the tenant ID associated with a specific domain by querying the OpenID configuration endpoint.
-        .PARAMETER Domain
-        Specifies the domain for which to retrieve the tenant ID.
-        .EXAMPLE
-        Get-O365TenantID -Domain 'evotec.pl'
-        .NOTES
-        For more information, refer to the OpenID Connect Discovery documentation.
+    .SYNOPSIS
+    Provides the tenant ID for a given domain.
+
+    .DESCRIPTION
+    This function retrieves the tenant ID associated with a specific domain by querying the OpenID configuration endpoint.
+
+    .PARAMETER Domain
+    Specifies the domain for which to retrieve the tenant ID.
+
+    .EXAMPLE
+    Get-O365TenantID -Domain 'evotec.pl'
+
+    .NOTES
+    For more information, refer to the OpenID Connect Discovery documentation.
     #>
     [cmdletbinding()]
     param(
