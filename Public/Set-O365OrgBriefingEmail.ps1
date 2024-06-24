e0c6d5f2d2082c24966f606614d7095794fd25ce
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:27:56 -0500

    Update Set-O365OrgBriefingEmail.ps1
    updated help

--- Public/Set-O365OrgBriefingEmail.ps1
+++ Public/Set-O365OrgBriefingEmail.ps1
@@ -1,23 +1,17 @@
 ﻿function Set-O365OrgBriefingEmail {
     <#
-    .SYNOPSIS
-    Let people in your organization receive Briefing Email
-
-    .DESCRIPTION
-    Let people in your organization receive Briefing Email
-
-    .PARAMETER Headers
-    Parameter description
-
-    .PARAMETER SubscribeByDefault
-    Subscribes or unsubscribes people in your organization to receive Briefing Email
-
-    .EXAMPLE
-    An example
-
-    .NOTES
-    Users will receive Briefing email by default, but can unsubscribe at any time from their Briefing email or Briefing settings page. Email is only sent to users if their Office 365 language is English or Spanish.
-
+        .SYNOPSIS
+        Let people in your organization receive Briefing Email
+        .DESCRIPTION
+        Let people in your organization receive Briefing Email
+        .PARAMETER Headers
+        Parameter description
+        .PARAMETER SubscribeByDefault
+        Subscribes or unsubscribes people in your organization to receive Briefing Email
+        .EXAMPLE
+        An example
+        .NOTES
+        Users will receive Briefing email by default, but can unsubscribe at any time from their Briefing email or Briefing settings page. Email is only sent to users if their Office 365 language is English or Spanish.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
