e2f7fdcc744b1f8a7ff00cc5be6b1a8a8c42b8be
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:03:22 -0500

    Update Get-O365OrgGraphDataConnect.ps1
    added help

--- Public/Get-O365OrgGraphDataConnect.ps1
+++ Public/Get-O365OrgGraphDataConnect.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365OrgGraphDataConnect {
+    <#
+        .SYNOPSIS
+        Retrieves Graph Data Connect information for the organization.
+        .DESCRIPTION
+        This function retrieves Graph Data Connect information for the organization from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365OrgGraphDataConnect -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
