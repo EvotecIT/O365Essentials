af5adf8bd7463ea2ace49e81cb47ecadba1e2210
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:02:55 -0500

    Update Get-O365OrgHelpdeskInformation.ps1
    added help

--- Public/Get-O365OrgHelpdeskInformation.ps1
+++ Public/Get-O365OrgHelpdeskInformation.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365OrgHelpdeskInformation {
+    <#
+        .SYNOPSIS
+        Retrieves helpdesk information for the organization.
+        .DESCRIPTION
+        This function retrieves helpdesk information for the organization from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365OrgHelpdeskInformation -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
