f0bb94228aa3460ec8e3e371ec4a1e35bbfd3661
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:50:16 -0500

    Update Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
    updated help formatting

--- Public/Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
+++ Public/Get-O365AzureEnterpriseAppsUserSettingsAdmin.ps1
@@ -1,18 +1,23 @@
 ﻿function Get-O365AzureEnterpriseAppsUserSettingsAdmin {
     <#
-        .SYNOPSIS
-        Retrieves user settings for Azure Enterprise Apps with admin consent flow.
-        .DESCRIPTION
-        This function retrieves user settings for Azure Enterprise Apps with admin consent flow based on the provided headers.
-        It returns information about request expiration days, notifications, reminders, approvers, and approversV2.
-        .PARAMETER Headers
-        A dictionary containing the headers for the API request, typically including authorization information.
-        .PARAMETER NoTranslation
-        Specifies whether to return the user settings without translation.
-        .EXAMPLE
-        Get-O365AzureEnterpriseAppsUserSettingsAdmin -Headers $headers
-        .NOTES
-        For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    .SYNOPSIS
+    Retrieves user settings for Azure Enterprise Apps with admin consent flow.
+
+    .DESCRIPTION
+    This function retrieves user settings for Azure Enterprise Apps with admin consent flow based on the provided headers.
+    It returns information about request expiration days, notifications, reminders, approvers, and approversV2.
+
+    .PARAMETER Headers
+    A dictionary containing the headers for the API request, typically including authorization information.
+
+    .PARAMETER NoTranslation
+    Specifies whether to return the user settings without translation.
+
+    .EXAMPLE
+    Get-O365AzureEnterpriseAppsUserSettingsAdmin -Headers $headers
+
+    .NOTES
+    For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
     #>
     [cmdletbinding()]
     param(
