be39c06645241138820df1aeed1aef50f9034d85
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:45:54 -0500

    Update Get-O365AzureMultiFactorAuthentication.ps1
    updated help formatting

--- Public/Get-O365AzureMultiFactorAuthentication.ps1
+++ Public/Get-O365AzureMultiFactorAuthentication.ps1
@@ -1,16 +1,20 @@
 ﻿function Get-O365AzureMultiFactorAuthentication {
     <#
-        .SYNOPSIS
-        Retrieves the Multi-Factor Authentication settings for the specified tenant.
-        .DESCRIPTION
-        This function retrieves the Multi-Factor Authentication settings for the specified tenant using the provided headers.
-        .PARAMETER Headers
-        Authorization header as created by Connect-O365Admin. If not provided the function will try to fetch it from the current execution context.
-        .EXAMPLE
-        Get-O365AzureMultiFactorAuthentication -Verbose
-        An example of how to retrieve Multi-Factor Authentication settings for a tenant.
-        .NOTES
-        Based on: https://portal.azure.com/#blade/Microsoft_AAD_IAM/MultifactorAuthenticationMenuBlade/GettingStarted/fromProviders/
+    .SYNOPSIS
+    Retrieves the Multi-Factor Authentication settings for the specified tenant.
+
+    .DESCRIPTION
+    This function retrieves the Multi-Factor Authentication settings for the specified tenant using the provided headers.
+
+    .PARAMETER Headers
+    Authorization header as created by Connect-O365Admin. If not provided the function will try to fetch it from the current execution context.
+
+    .EXAMPLE
+    Get-O365AzureMultiFactorAuthentication -Verbose
+    An example of how to retrieve Multi-Factor Authentication settings for a tenant.
+
+    .NOTES
+    Based on: https://portal.azure.com/#blade/Microsoft_AAD_IAM/MultifactorAuthenticationMenuBlade/GettingStarted/fromProviders/
     #>
     [cmdletbinding()]
     param(
