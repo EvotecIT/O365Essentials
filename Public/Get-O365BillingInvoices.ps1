ec6827ebd0f18aa930c8e04215cbaab703e1028b
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:27:13 -0500

    Update Get-O365BillingInvoices.ps1
    updated help

--- Public/Get-O365BillingInvoices.ps1
+++ Public/Get-O365BillingInvoices.ps1
@@ -1,25 +1,19 @@
 ﻿function Get-O365BillingInvoices {
     <#
-    .SYNOPSIS
-    Gets all invoices from Office 365. If no StartDate and EndDate are specified last 6 months are used.
-
-    .DESCRIPTION
-    Gets all invoices from Office 365. If no StartDate and EndDate are specified last 6 months are used.
-
-    .PARAMETER Headers
-    Parameter description
-
-    .PARAMETER StartDate
-    Provide StartDate for the invoices to be retrieved. If not specified, StartDate is set to 6 months ago.
-
-    .PARAMETER EndDate
-    Provide EndDate for the invoices to be retrieved. If not specified, EndDate is set to current date.
-
-    .EXAMPLE
-    An example
-
-    .NOTES
-    General notes
+        .SYNOPSIS
+        Gets all invoices from Office 365. If no StartDate and EndDate are specified last 6 months are used.
+        .DESCRIPTION
+        Gets all invoices from Office 365. If no StartDate and EndDate are specified last 6 months are used.
+        .PARAMETER Headers
+        Parameter description
+        .PARAMETER StartDate
+        Provide StartDate for the invoices to be retrieved. If not specified, StartDate is set to 6 months ago.
+        .PARAMETER EndDate
+        Provide EndDate for the invoices to be retrieved. If not specified, EndDate is set to current date.
+        .EXAMPLE
+        Get-O365BillingInvoices -Headers $headers -StartDate (Get-Date).AddMonths(-6) -EndDate (Get-Date)
+        .NOTES
+        This function retrieves invoices from Office 365. If no specific StartDate and EndDate are provided, the function defaults to retrieving invoices from the last 6 months.
     #>
     [cmdletbinding()]
     param(
