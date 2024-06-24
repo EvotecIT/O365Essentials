02f3c2c6516dfd999e5f916b7576867202a7b160
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:14:53 -0500

    Update Set-O365OrgProject.ps1
    added help

--- Public/Set-O365OrgProject.ps1
+++ Public/Set-O365OrgProject.ps1
@@ -1,4 +1,21 @@
 ﻿function Set-O365OrgProject {
+    <#
+        .SYNOPSIS
+        Configures the project settings for an Office 365 organization.
+        .DESCRIPTION
+        This function updates the project settings for an Office 365 organization. It allows enabling or disabling Roadmap and Project for the Web features.
+        .PARAMETER Headers
+        Specifies the headers for the API request. Typically includes authorization tokens.
+        .PARAMETER RoadmapEnabled
+        Specifies whether the Roadmap feature should be enabled or disabled. Accepts a nullable boolean value.
+        .PARAMETER ProjectForTheWebEnabled
+        Specifies whether the Project for the Web feature should be enabled or disabled. Accepts a nullable boolean value.
+        .EXAMPLE
+        $headers = @{Authorization = "Bearer your_token"}
+        Set-O365OrgProject -Headers $headers -RoadmapEnabled $true -ProjectForTheWebEnabled $false
+
+        This example enables the Roadmap feature and disables the Project for the Web feature.
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
