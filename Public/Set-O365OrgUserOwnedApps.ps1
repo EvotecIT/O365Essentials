25779e695e17467a8c3c6b829c5c5174c7790b25
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:08:51 -0500

    Update Set-O365OrgUserOwnedApps.ps1
    added help

--- Public/Set-O365OrgUserOwnedApps.ps1
+++ Public/Set-O365OrgUserOwnedApps.ps1
@@ -1,4 +1,23 @@
 ﻿function Set-O365OrgUserOwnedApps {
+    <#
+        .SYNOPSIS
+        Configures settings for user-owned apps in Office 365.
+        .DESCRIPTION
+        This function updates the configuration settings for user-owned apps in Office 365. It allows enabling or disabling user access to the Office Store, starting trials, and auto-claiming licenses.
+        .PARAMETER Headers
+        Specifies the headers for the API request. Typically includes authorization tokens.
+        .PARAMETER LetUsersAccessOfficeStore
+        Specifies whether users are allowed to access the Office Store.
+        .PARAMETER LetUsersStartTrials
+        Specifies whether users are allowed to start trials.
+        .PARAMETER LetUsersAutoClaimLicenses
+        Specifies whether users are allowed to auto-claim licenses.
+        .EXAMPLE
+        $headers = @{Authorization = "Bearer your_token"}
+        Set-O365OrgUserOwnedApps -Headers $headers -LetUsersAccessOfficeStore $true -LetUsersStartTrials $false -LetUsersAutoClaimLicenses $true
+
+        This example enables user access to the Office Store, disables starting trials, and enables auto-claiming licenses.
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
