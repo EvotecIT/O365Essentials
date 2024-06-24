3897da801e30106909fb02de0c7ace9e31b7d54d
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:06:32 -0500

    Update Get-O365OrgCustomThemes.ps1
    added help

--- Public/Get-O365OrgCustomThemes.ps1
+++ Public/Get-O365OrgCustomThemes.ps1
@@ -1,4 +1,17 @@
 ﻿function Get-O365OrgCustomThemes {
+    <#
+        .SYNOPSIS
+        Retrieves custom themes information for the organization.
+        .DESCRIPTION
+        This function retrieves custom themes information for the organization from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        Specifies the headers containing the authorization information.
+        .EXAMPLE
+        Get-O365OrgCustomThemes -Headers $Headers
+        An example of how to retrieve custom themes information.
+        .NOTES
+        General notes
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
