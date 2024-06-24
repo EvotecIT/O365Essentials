5a2ba69b1610a4cafcf222f636dd6e3404719361
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:37:58 -0500

    Update Get-O365DomainTroubleshooting.ps1
    updated help formatting

--- Public/Get-O365DomainTroubleshooting.ps1
+++ Public/Get-O365DomainTroubleshooting.ps1
@@ -1,15 +1,19 @@
 ﻿function Get-O365DomainTroubleshooting {
     <#
-        .SYNOPSIS
-        Provides troubleshooting information for a specified domain in Office 365.
-        .DESCRIPTION
-        This function allows you to check if troubleshooting is allowed for a specific domain in Office 365 using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .PARAMETER DomainName
-        The name of the domain to check troubleshooting for.
-        .EXAMPLE
-        Get-O365DomainTroubleshooting -Headers $headers -DomainName 'example.com'
+    .SYNOPSIS
+    Provides troubleshooting information for a specified domain in Office 365.
+
+    .DESCRIPTION
+    This function allows you to check if troubleshooting is allowed for a specific domain in Office 365 using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .PARAMETER DomainName
+    The name of the domain to check troubleshooting for.
+
+    .EXAMPLE
+    Get-O365DomainTroubleshooting -Headers $headers -DomainName 'example.com'
     #>
     [cmdletbinding()]
     param(
