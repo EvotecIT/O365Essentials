1418a0fd8e5250e00c943963cc424e5ec42e119b
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:32:30 -0500

    Update Get-O365OrgCustomThemes.ps1
    updated help formatting

--- Public/Get-O365OrgCustomThemes.ps1
+++ Public/Get-O365OrgCustomThemes.ps1
@@ -1,16 +1,20 @@
 ﻿function Get-O365OrgCustomThemes {
     <#
-        .SYNOPSIS
-        Retrieves custom themes information for the organization.
-        .DESCRIPTION
-        This function retrieves custom themes information for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        Specifies the headers containing the authorization information.
-        .EXAMPLE
-        Get-O365OrgCustomThemes -Headers $Headers
-        An example of how to retrieve custom themes information.
-        .NOTES
-        General notes
+    .SYNOPSIS
+    Retrieves custom themes information for the organization.
+
+    .DESCRIPTION
+    This function retrieves custom themes information for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    Specifies the headers containing the authorization information.
+
+    .EXAMPLE
+    Get-O365OrgCustomThemes -Headers $Headers
+    An example of how to retrieve custom themes information.
+
+    .NOTES
+    General notes
     #>
     [cmdletbinding()]
     param(
