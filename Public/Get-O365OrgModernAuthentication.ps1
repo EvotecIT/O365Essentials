30ad39015363d1f78c3bb3cdacb3e7fc4cb475aa
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:27:54 -0500

    Update Get-O365OrgModernAuthentication.ps1
    updated help formatting

--- Public/Get-O365OrgModernAuthentication.ps1
+++ Public/Get-O365OrgModernAuthentication.ps1
@@ -1,15 +1,19 @@
 ﻿function Get-O365OrgModernAuthentication {
     <#
-        .SYNOPSIS
-        Provides information about modern authentication for Office 365.
-        .DESCRIPTION
-        This function retrieves details about modern authentication for Office 365 from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365OrgModernAuthentication -Verbose
-        .NOTES
-        For more information, visit: https://admin.microsoft.com/#/Settings/Services/:/Settings/L1/ModernAuthentication
+    .SYNOPSIS
+    Provides information about modern authentication for Office 365.
+
+    .DESCRIPTION
+    This function retrieves details about modern authentication for Office 365 from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365OrgModernAuthentication -Verbose
+
+    .NOTES
+    For more information, visit: https://admin.microsoft.com/#/Settings/Services/:/Settings/L1/ModernAuthentication
     #>
     [cmdletbinding()]
     param(
