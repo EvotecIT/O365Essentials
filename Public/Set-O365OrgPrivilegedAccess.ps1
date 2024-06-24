3378b9dc3f2384e5b34773b444438d8c8958c1d1
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 11:50:51 -0500

    Update Set-O365OrgPrivilegedAccess.ps1
    updated help formatting

--- Public/Set-O365OrgPrivilegedAccess.ps1
+++ Public/Set-O365OrgPrivilegedAccess.ps1
@@ -1,20 +1,25 @@
 ﻿function Set-O365OrgPrivilegedAccess {
     <#
-        .SYNOPSIS
-        Configures the privileged access settings for an Office 365 organization.
-        .DESCRIPTION
-        This function updates the privileged access settings for an Office 365 organization. It allows enabling or disabling the Tenant Lockbox feature and specifying an admin group.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER TenantLockBoxEnabled
-        Specifies whether the Tenant Lockbox feature should be enabled or disabled. Accepts a nullable boolean value.
-        .PARAMETER AdminGroup
-        Specifies the admin group for privileged access.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgPrivilegedAccess -Headers $headers -TenantLockBoxEnabled $true -AdminGroup "AdminGroupName"
+    .SYNOPSIS
+    Configures the privileged access settings for an Office 365 organization.
 
-        This example enables the Tenant Lockbox feature and sets the admin group to "AdminGroupName".
+    .DESCRIPTION
+    This function updates the privileged access settings for an Office 365 organization. It allows enabling or disabling the Tenant Lockbox feature and specifying an admin group.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER TenantLockBoxEnabled
+    Specifies whether the Tenant Lockbox feature should be enabled or disabled. Accepts a nullable boolean value.
+
+    .PARAMETER AdminGroup
+    Specifies the admin group for privileged access.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgPrivilegedAccess -Headers $headers -TenantLockBoxEnabled $true -AdminGroup "AdminGroupName"
+
+    This example enables the Tenant Lockbox feature and sets the admin group to "AdminGroupName".
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
