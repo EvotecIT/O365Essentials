64e7be0bf6aadf1bd5d8588ea236c66e8d8294c3
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:31:17 -0500

    Update Get-O365OrgDynamics365ConnectionGraph.ps1
    updated help formatting

--- Public/Get-O365OrgDynamics365ConnectionGraph.ps1
+++ Public/Get-O365OrgDynamics365ConnectionGraph.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-O365OrgDynamics365ConnectionGraph {
     <#
-        .SYNOPSIS
-        Retrieves Dynamics 365 Connection Graph information for the organization.
-        .DESCRIPTION
-        This function retrieves Dynamics 365 Connection Graph information for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365OrgDynamics365ConnectionGraph -Headers $headers
+    .SYNOPSIS
+    Retrieves Dynamics 365 Connection Graph information for the organization.
+
+    .DESCRIPTION
+    This function retrieves Dynamics 365 Connection Graph information for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365OrgDynamics365ConnectionGraph -Headers $headers
     #>
     [cmdletbinding()]
     param(
