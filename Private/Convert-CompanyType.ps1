9743a197ef2d0518ac9a294866325c03c201bc16
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 10:49:29 -0500

    Update Convert-CompanyType.ps1
    updated help formatting

--- Private/Convert-CompanyType.ps1
+++ Private/Convert-CompanyType.ps1
@@ -1,28 +1,28 @@
 ﻿function Convert-CompanyType {
     <#
-        .SYNOPSIS
-        Converts company type codes to their descriptive names.
+    .SYNOPSIS
+    Converts company type codes to their descriptive names.
 
-        .DESCRIPTION
-        This function takes an array of company type codes and converts each code to its corresponding descriptive name. 
-        If a code does not have a corresponding name, the code itself is returned.
+    .DESCRIPTION
+    This function takes an array of company type codes and converts each code to its corresponding descriptive name. 
+    If a code does not have a corresponding name, the code itself is returned.
+
+    .PARAMETER CompanyType
+    An array of company type codes that need to be converted to descriptive names.
+
+    .EXAMPLE
+    Convert-CompanyType -CompanyType '5', '4'
+    # Returns 'Indirect reseller', 'Reseller'
         
-        .PARAMETER CompanyType
-        An array of company type codes that need to be converted to descriptive names.
+    .EXAMPLE
+    Convert-CompanyType -CompanyType '5', '1'
+    # Returns 'Indirect reseller', '1'
+    # Note: '1' is returned as is because it does not have a corresponding name in the mapping.
         
-        .EXAMPLE
-        Convert-CompanyType -CompanyType '5', '4'
-        # Returns 'Indirect reseller', 'Reseller'
-        
-        .EXAMPLE
-        Convert-CompanyType -CompanyType '5', '1'
-        # Returns 'Indirect reseller', '1'
-        # Note: '1' is returned as is because it does not have a corresponding name in the mapping.
-        
-        .NOTES
-        Current mappings include:
-        '5' for 'Indirect reseller'
-        '4' for 'Reseller'
+    .NOTES
+    Current mappings include:
+    '5' for 'Indirect reseller'
+    '4' for 'Reseller'
     #>
     [cmdletbinding()]
     param(
