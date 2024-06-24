f74f94de19b58736ac9e5bc6545e5ed856747060
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:26:02 -0500

    Update Set-O365OrgCortana.ps1
    updated help formatting

--- Public/Set-O365OrgCortana.ps1
+++ Public/Set-O365OrgCortana.ps1
@@ -1,21 +1,26 @@
 ﻿function Set-O365OrgCortana {
     <#
-        .SYNOPSIS
-        Configures the Cortana settings for an Office 365 organization.
-        .DESCRIPTION
-        This function allows you to enable or disable the Cortana service for your Office 365 organization.
-        It sends a POST request to the Office 365 admin API with the specified settings.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER Enabled
-        Specifies whether the Cortana service should be enabled.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgCortana -Headers $headers -Enabled $true
+    .SYNOPSIS
+    Configures the Cortana settings for an Office 365 organization.
 
-        This example enables the Cortana service for the Office 365 organization.
-        .NOTES
-        This function sends a POST request to the Office 365 admin API with the specified settings.
+    .DESCRIPTION
+    This function allows you to enable or disable the Cortana service for your Office 365 organization.
+    It sends a POST request to the Office 365 admin API with the specified settings.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER Enabled
+    Specifies whether the Cortana service should be enabled.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgCortana -Headers $headers -Enabled $true
+
+    This example enables the Cortana service for the Office 365 organization.
+
+    .NOTES
+    This function sends a POST request to the Office 365 admin API with the specified settings.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
