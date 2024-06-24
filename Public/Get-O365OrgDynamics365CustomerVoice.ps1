39042367c145e8f9ee9cf7834cf5ad3b234cde0c
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:30:56 -0500

    Update Get-O365OrgDynamics365CustomerVoice.ps1
    updated help formatting

--- Public/Get-O365OrgDynamics365CustomerVoice.ps1
+++ Public/Get-O365OrgDynamics365CustomerVoice.ps1
@@ -1,13 +1,16 @@
 ﻿function Get-O365OrgDynamics365CustomerVoice {
     <#
-        .SYNOPSIS
-        Retrieves Dynamics 365 Customer Voice information for the organization.
-        .DESCRIPTION
-        This function retrieves Dynamics 365 Customer Voice information for the organization from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365OrgDynamics365CustomerVoice -Headers $headers
+    .SYNOPSIS
+    Retrieves Dynamics 365 Customer Voice information for the organization.
+
+    .DESCRIPTION
+    This function retrieves Dynamics 365 Customer Voice information for the organization from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365OrgDynamics365CustomerVoice -Headers $headers
     #>
     [cmdletbinding()]
     param(
