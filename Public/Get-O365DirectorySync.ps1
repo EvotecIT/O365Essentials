237393138b3b2cdf792cde6a8ac8751f3c78f839
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:17:37 -0500

    Update Get-O365DirectorySync.ps1
    added help

--- Public/Get-O365DirectorySync.ps1
+++ Public/Get-O365DirectorySync.ps1
@@ -1,4 +1,12 @@
 ﻿function Get-O365DirectorySync {
+    <#
+        .SYNOPSIS
+        Retrieves directory synchronization settings from Office 365.
+        .DESCRIPTION
+        This function retrieves directory synchronization settings from Office 365 using the specified API endpoint and headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
