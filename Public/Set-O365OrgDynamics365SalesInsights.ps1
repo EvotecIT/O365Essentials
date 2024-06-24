ef50f92c2bf493663aa3821f9b890806c7408abe
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:24:50 -0500

    Update Set-O365OrgDynamics365SalesInsights.ps1
    added help

--- Public/Set-O365OrgDynamics365SalesInsights.ps1
+++ Public/Set-O365OrgDynamics365SalesInsights.ps1
@@ -1,4 +1,22 @@
 ﻿function Set-O365OrgDynamics365SalesInsights {
+    <#
+        .SYNOPSIS
+        Configures the Dynamics 365 Sales Insights settings for an Office 365 organization.
+        .DESCRIPTION
+        This function allows you to enable or disable the Dynamics 365 Sales Insights service for your Office 365 organization. 
+        It sends a POST request to the Office 365 admin API with the specified settings.
+        .PARAMETER Headers
+        Specifies the headers for the API request. Typically includes authorization tokens.
+        .PARAMETER ServiceEnabled
+        Specifies whether the Dynamics 365 Sales Insights service should be enabled.
+        .EXAMPLE
+        $headers = @{Authorization = "Bearer your_token"}
+        Set-O365OrgDynamics365SalesInsights -Headers $headers -ServiceEnabled $true
+
+        This example enables the Dynamics 365 Sales Insights service for the Office 365 organization.
+        .NOTES
+        This function sends a POST request to the Office 365 admin API with the specified settings.
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
