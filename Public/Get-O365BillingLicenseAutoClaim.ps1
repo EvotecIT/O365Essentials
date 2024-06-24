727d87832f630231a46a00d55b27bda3165d63ee
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:43:53 -0500

    Update Get-O365BillingLicenseAutoClaim.ps1
    updated help formatting

--- Public/Get-O365BillingLicenseAutoClaim.ps1
+++ Public/Get-O365BillingLicenseAutoClaim.ps1
@@ -16,15 +16,19 @@ function Get-O365BillingLicenseAutoClaim {
 #>
 function Get-O365BillingLicenseAutoClaim {
     <#
-        .SYNOPSIS
-        Retrieves information about licensed devices assets for a specific Office 365 tenant.
-        .DESCRIPTION
-        This function retrieves information about licensed devices assets for a specific Office 365 tenant using the provided headers.
-        .PARAMETER Headers
-        A dictionary containing the necessary headers for the API request, typically including authorization information.
-        .EXAMPLE
-        Get-O365BillingLicenseAutoClaim -Headers $headers
-    #>    [cmdletbinding()]
+    .SYNOPSIS
+    Retrieves information about licensed devices assets for a specific Office 365 tenant.
+
+    .DESCRIPTION
+    This function retrieves information about licensed devices assets for a specific Office 365 tenant using the provided headers.
+
+    .PARAMETER Headers
+    A dictionary containing the necessary headers for the API request, typically including authorization information.
+
+    .EXAMPLE
+    Get-O365BillingLicenseAutoClaim -Headers $headers
+    #>
+    [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
     )
