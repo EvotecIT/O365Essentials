7f77e5bc8ad3a0c205e2b20f37419edc51fdd2fc
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:25:52 -0500

    Update Get-O365OrgOrganizationInformation.ps1
    updated help formatting

--- Public/Get-O365OrgOrganizationInformation.ps1
+++ Public/Get-O365OrgOrganizationInformation.ps1
@@ -1,17 +1,22 @@
 ﻿function Get-O365OrgOrganizationInformation {
     <#
-        .SYNOPSIS
-        Retrieves organization information from the specified URI.
-        .DESCRIPTION
-        This function retrieves organization information from the specified URI using the provided headers.
-        .PARAMETER Headers
-        Authentication token and additional information for the API request.
-        .PARAMETER NoTranslation
-        Specifies whether to skip translation.
-        .EXAMPLE
-        Get-O365OrgOrganizationInformation -Headers $headers -NoTranslation
-        .NOTES
-        This function retrieves organization information from the specified URI.
+    .SYNOPSIS
+    Retrieves organization information from the specified URI.
+
+    .DESCRIPTION
+    This function retrieves organization information from the specified URI using the provided headers.
+
+    .PARAMETER Headers
+    Authentication token and additional information for the API request.
+
+    .PARAMETER NoTranslation
+    Specifies whether to skip translation.
+
+    .EXAMPLE
+    Get-O365OrgOrganizationInformation -Headers $headers -NoTranslation
+
+    .NOTES
+    This function retrieves organization information from the specified URI.
     #>
     [alias('Get-O365OrgCompanyInformation')]
     [cmdletbinding()]
