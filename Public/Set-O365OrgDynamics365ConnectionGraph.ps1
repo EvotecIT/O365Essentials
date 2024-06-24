0c58455f9b0ec4257713d15b0b94679174e8504c
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 12:24:54 -0500

    Update Set-O365OrgDynamics365ConnectionGraph.ps1
    updated help formatting

--- Public/Set-O365OrgDynamics365ConnectionGraph.ps1
+++ Public/Set-O365OrgDynamics365ConnectionGraph.ps1
@@ -1,23 +1,29 @@
 ﻿function Set-O365OrgDynamics365ConnectionGraph {
     <#
-        .SYNOPSIS
-        Configures the Dynamics 365 Connection Graph settings for an Office 365 organization.
-        .DESCRIPTION
-        This function allows you to enable or disable the Dynamics 365 Connection Graph service for your Office 365 organization.
-        It sends a POST request to the Office 365 admin API with the specified settings.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER ServiceEnabled
-        Specifies whether the Dynamics 365 Connection Graph service should be enabled.
-        .PARAMETER ConnectionGraphUsersExclusionGroup
-        Specifies the group of users to be excluded from the Connection Graph.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgDynamics365ConnectionGraph -Headers $headers -ServiceEnabled $true -ConnectionGraphUsersExclusionGroup "GroupID"
+    .SYNOPSIS
+    Configures the Dynamics 365 Connection Graph settings for an Office 365 organization.
 
-        This example enables the Dynamics 365 Connection Graph service for the Office 365 organization and excludes the specified group of users.
-        .NOTES
-        This function sends a POST request to the Office 365 admin API with the specified settings.
+    .DESCRIPTION
+    This function allows you to enable or disable the Dynamics 365 Connection Graph service for your Office 365 organization.
+    It sends a POST request to the Office 365 admin API with the specified settings.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER ServiceEnabled
+    Specifies whether the Dynamics 365 Connection Graph service should be enabled.
+
+    .PARAMETER ConnectionGraphUsersExclusionGroup
+    Specifies the group of users to be excluded from the Connection Graph.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgDynamics365ConnectionGraph -Headers $headers -ServiceEnabled $true -ConnectionGraphUsersExclusionGroup "GroupID"
+
+    This example enables the Dynamics 365 Connection Graph service for the Office 365 organization and excludes the specified group of users.
+
+    .NOTES
+    This function sends a POST request to the Office 365 admin API with the specified settings.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
