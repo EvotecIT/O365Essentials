4ef7b03b11f273d6dca6aa41d0472611180749c7
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 08:40:45 -0500

    Update Set-O365AzureEnterpriseAppsUserConsent.ps1
    added help

--- Public/Set-O365AzureEnterpriseAppsUserConsent.ps1
+++ Public/Set-O365AzureEnterpriseAppsUserConsent.ps1
@@ -1,5 +1,19 @@
 ﻿function Set-O365AzureEnterpriseAppsUserConsent {
-    # https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
+    <#
+        .SYNOPSIS
+        Configures user consent settings for Azure enterprise applications.
+        .DESCRIPTION
+        This function allows administrators to configure user consent settings for Azure enterprise applications.
+        .PARAMETER Headers
+        Specifies the headers for the API request, typically including authorization tokens.
+        .PARAMETER PermissionGrantPoliciesAssigned
+        Specifies the permission grant policies assigned for user consent.
+        .EXAMPLE
+        An example of how to use this function:
+        Set-O365AzureEnterpriseAppsUserConsent -Headers $headers -PermissionGrantPoliciesAssigned 'AllowUserConsentForApps'
+        .NOTES
+        For more information, visit: https://portal.azure.com/#blade/Microsoft_AAD_IAM/ConsentPoliciesMenuBlade/UserSettings
+    #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
