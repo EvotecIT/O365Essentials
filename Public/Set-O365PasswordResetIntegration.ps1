67e0930128d0957e9ddabcf61159244f366c942e
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 10:56:43 -0500

    Update Set-O365PasswordResetIntegration.ps1
    updated help formatting

--- Public/Set-O365PasswordResetIntegration.ps1
+++ Public/Set-O365PasswordResetIntegration.ps1
@@ -1,21 +1,26 @@
 ﻿function Set-O365PasswordResetIntegration {
-     <#
-        .SYNOPSIS
-        Configures password reset integration settings for Office 365.
-        .DESCRIPTION
-        This function updates the settings for password writeback and account unlock features in Office 365. 
-        It allows enabling or disabling the password writeback and account unlock capabilities.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER PasswordWritebackSupported
-        Indicates whether password writeback is supported. Accepts $true or $false.
-        .PARAMETER AccountUnlockEnabled
-        Indicates whether account unlock is enabled. Accepts $true or $false.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365PasswordResetIntegration -Headers $headers -PasswordWritebackSupported $true -AccountUnlockEnabled $true
+    <#
+    .SYNOPSIS
+    Configures password reset integration settings for Office 365.
+
+    .DESCRIPTION
+    This function updates the settings for password writeback and account unlock features in Office 365. 
+    It allows enabling or disabling the password writeback and account unlock capabilities.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER PasswordWritebackSupported
+    Indicates whether password writeback is supported. Accepts $true or $false.
+
+    .PARAMETER AccountUnlockEnabled
+    Indicates whether account unlock is enabled. Accepts $true or $false.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365PasswordResetIntegration -Headers $headers -PasswordWritebackSupported $true -AccountUnlockEnabled $true
     
-        This example enables both password writeback and account unlock features.
+    This example enables both password writeback and account unlock features.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
