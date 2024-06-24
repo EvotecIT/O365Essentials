ef6a43bf645dfbdeb865579bd0f4f04912a52616
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 11:53:09 -0500

    Update Set-O365OrgPasswordExpirationPolicy.ps1
    updated help formatting

--- Public/Set-O365OrgPasswordExpirationPolicy.ps1
+++ Public/Set-O365OrgPasswordExpirationPolicy.ps1
@@ -1,23 +1,29 @@
 ﻿function Set-O365OrgPasswordExpirationPolicy {
     <#
-        .SYNOPSIS
-        Configures the password expiration policy for an Office 365 organization.
-        .DESCRIPTION
-        This function updates the password expiration policy settings for an Office 365 organization. It allows specifying whether passwords never expire, 
-        the number of days before passwords expire, and the number of days before users are notified of password expiration.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER PasswordNeverExpires
-        Specifies whether passwords should never expire. Accepts a nullable boolean value.
-        .PARAMETER DaysBeforePasswordExpires
-        Specifies the number of days before passwords expire. Accepts a nullable integer value.
-        .PARAMETER DaysBeforeUserNotified
-        Specifies the number of days before users are notified of password expiration. Accepts a nullable integer value.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgPasswordExpirationPolicy -Headers $headers -PasswordNeverExpires $true -DaysBeforePasswordExpires 90 -DaysBeforeUserNotified 14
+    .SYNOPSIS
+    Configures the password expiration policy for an Office 365 organization.
 
-        This example sets the password expiration policy to never expire passwords, with a notification period of 14 days before expiration.
+    .DESCRIPTION
+    This function updates the password expiration policy settings for an Office 365 organization. It allows specifying whether passwords never expire, 
+    the number of days before passwords expire, and the number of days before users are notified of password expiration.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER PasswordNeverExpires
+    Specifies whether passwords should never expire. Accepts a nullable boolean value.
+
+    .PARAMETER DaysBeforePasswordExpires
+    Specifies the number of days before passwords expire. Accepts a nullable integer value.
+
+    .PARAMETER DaysBeforeUserNotified
+    Specifies the number of days before users are notified of password expiration. Accepts a nullable integer value.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgPasswordExpirationPolicy -Headers $headers -PasswordNeverExpires $true -DaysBeforePasswordExpires 90 -DaysBeforeUserNotified 14
+
+    This example sets the password expiration policy to never expire passwords, with a notification period of 14 days before expiration.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
