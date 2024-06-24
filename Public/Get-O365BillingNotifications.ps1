8d79ab3d2a1d4d056292a9afcaf97890bb04eabc
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:25:06 -0500

    Update Get-O365BillingNotifications.ps1
    added help

--- Public/Get-O365BillingNotifications.ps1
+++ Public/Get-O365BillingNotifications.ps1
@@ -1,4 +1,14 @@
 ﻿function Get-O365BillingNotifications {
+    <#
+        .SYNOPSIS
+        Retrieves invoice preference settings for billing notifications in Office 365.
+        .DESCRIPTION
+        This function retrieves invoice preference settings for billing notifications in Office 365 from the specified API endpoint using the provided headers.
+        .PARAMETER Headers
+        A dictionary containing the necessary headers for the API request, typically including authorization information.
+        .EXAMPLE
+        Get-O365BillingNotifications -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
