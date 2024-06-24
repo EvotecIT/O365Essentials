3401929fb5a82e09a449504ac702bdae05f4998b
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 13:17:39 -0500

    Update Get-O365SearchIntelligenceMeetingInsights.ps1
    updated help formatting

--- Public/Get-O365SearchIntelligenceMeetingInsights.ps1
+++ Public/Get-O365SearchIntelligenceMeetingInsights.ps1
@@ -1,11 +1,13 @@
 ﻿function Get-O365SearchIntelligenceMeetingInsights {
     <#
-        .SYNOPSIS
-        Retrieves meeting insights for Office 365 search intelligence.
-        .DESCRIPTION
-        This function retrieves meeting insights for Office 365 search intelligence from the specified API endpoint using the provided headers.
-        .PARAMETER Headers
-        Authentication token and additional information for the API request.
+    .SYNOPSIS
+    Retrieves meeting insights for Office 365 search intelligence.
+
+    .DESCRIPTION
+    This function retrieves meeting insights for Office 365 search intelligence from the specified API endpoint using the provided headers.
+
+    .PARAMETER Headers
+    Authentication token and additional information for the API request.
     #>
     [cmdletbinding()]
     param(
