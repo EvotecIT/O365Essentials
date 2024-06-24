05ef4e1777cadeb4fd3716a55909ec61d088f193
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 11:02:49 -0500

    Update Set-O365OrgTodo.ps1
    updated help formatting

--- Public/Set-O365OrgTodo.ps1
+++ Public/Set-O365OrgTodo.ps1
@@ -1,22 +1,28 @@
 ﻿function Set-O365OrgTodo {
     <#
-        .SYNOPSIS
-        Configures settings for Microsoft To-Do in Office 365.
-        .DESCRIPTION
-        This function updates the configuration settings for Microsoft To-Do in Office 365. It allows enabling or disabling external join, push notifications, and external sharing.
-        .PARAMETER Headers
-        Specifies the headers for the API request. Typically includes authorization tokens.
-        .PARAMETER ExternalJoinEnabled
-        Specifies whether external join is enabled or disabled.
-        .PARAMETER PushNotificationEnabled
-        Specifies whether push notifications are enabled or disabled.
-        .PARAMETER ExternalShareEnabled
-        Specifies whether external sharing is enabled or disabled.
-        .EXAMPLE
-        $headers = @{Authorization = "Bearer your_token"}
-        Set-O365OrgTodo -Headers $headers -ExternalJoinEnabled $true -PushNotificationEnabled $false -ExternalShareEnabled $true
+    .SYNOPSIS
+    Configures settings for Microsoft To-Do in Office 365.
 
-        This example enables external join, disables push notifications, and enables external sharing for Microsoft To-Do.
+    .DESCRIPTION
+    This function updates the configuration settings for Microsoft To-Do in Office 365. It allows enabling or disabling external join, push notifications, and external sharing.
+
+    .PARAMETER Headers
+    Specifies the headers for the API request. Typically includes authorization tokens.
+
+    .PARAMETER ExternalJoinEnabled
+    Specifies whether external join is enabled or disabled.
+
+    .PARAMETER PushNotificationEnabled
+    Specifies whether push notifications are enabled or disabled.
+
+    .PARAMETER ExternalShareEnabled
+    Specifies whether external sharing is enabled or disabled.
+
+    .EXAMPLE
+    $headers = @{Authorization = "Bearer your_token"}
+    Set-O365OrgTodo -Headers $headers -ExternalJoinEnabled $true -PushNotificationEnabled $false -ExternalShareEnabled $true
+
+    This example enables external join, disables push notifications, and enables external sharing for Microsoft To-Do.
     #>
     [cmdletbinding(SupportsShouldProcess)]
     param(
