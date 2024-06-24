a052101e4ef4bfa42fcf0c9c0dacac606913d465
neztach <36271369+neztach@users.noreply.github.com>
2024-06-24 09:39:04 -0500

    Update Get-O365AzureEnterpriseAppsGroupConsent.ps1
    added help

--- Public/Get-O365AzureEnterpriseAppsGroupConsent.ps1
+++ Public/Get-O365AzureEnterpriseAppsGroupConsent.ps1
@@ -1,4 +1,19 @@
 ﻿function Get-O365AzureEnterpriseAppsGroupConsent {
+    <#
+        .SYNOPSIS
+        Retrieves the group consent settings for Azure Enterprise Apps.
+        .DESCRIPTION
+        This function retrieves the group consent settings for Azure Enterprise Apps from the Microsoft Graph API.
+        It returns the specific consent settings for the group.
+        .PARAMETER Headers
+        A dictionary containing the headers for the API request, typically including authorization information.
+        .PARAMETER NoTranslation
+        Specifies whether to return the consent settings without translation.
+        .EXAMPLE
+        Get-O365AzureEnterpriseAppsGroupConsent -Headers $headers
+        .NOTES
+        For more information, visit: https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
+    #>
     # https://docs.microsoft.com/en-us/azure/active-directory/manage-apps/configure-user-consent?tabs=azure-portal
     [cmdletbinding()]
     param(
