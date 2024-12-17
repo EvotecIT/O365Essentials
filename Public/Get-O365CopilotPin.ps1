9228d6b258b2a268d8ddd83a5556326a9e6a8da9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-17 21:04:35 +0100

    Add Get-O365CopilotPin function and example script for retrieving Copilot PIN

---
+++ Public/Get-O365CopilotPin.ps1
@@ -0,0 +1,9 @@
+﻿function Get-O365CopilotPin {
+    [cmdletbinding()]
+    param(
+        [alias('Authorization')][System.Collections.IDictionary] $Headers
+    )
+    $Uri = "https://admin.microsoft.com/admin/api/settings/company/copilotpolicy/pin"
+    $OutputSettings = Invoke-O365Admin -Uri $Uri -Headers $Headers -Method GET
+    $OutputSettings
+}
