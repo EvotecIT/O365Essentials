0e47fafd7ec95d44a182ec976544dc1bcc89a3ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-23 09:49:28 +0100

    Update docs

--- Public/Get-O365CopilotPin.ps1
+++ Public/Get-O365CopilotPin.ps1
@@ -1,4 +1,17 @@
 ﻿function Get-O365CopilotPin {
+    <#
+    .SYNOPSIS
+    Retrieves the Copilot PIN settings for the organization.
+
+    .DESCRIPTION
+    The Get-O365CopilotPin function sends a GET request to the Microsoft 365 admin API to retrieve the Copilot PIN settings for the organization.
+
+    .PARAMETER Headers
+    The authorization headers required to authenticate the API request.
+
+    .EXAMPLE
+    Get-O365CopilotPin -Headers $headers
+    #>
     [cmdletbinding()]
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
