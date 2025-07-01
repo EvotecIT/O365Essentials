c64ef53f488db255154b513b72debfb8d286def9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 20:35:01 +0200

    Update Get-O365BillingAccounts.ps1 to change the API endpoint for retrieving billing accounts, improving accuracy in data retrieval.

--- Public/Get-O365BillingAccounts.ps1
+++ Public/Get-O365BillingAccounts.ps1
@@ -16,7 +16,7 @@
     param(
         [alias('Authorization')][System.Collections.IDictionary] $Headers
     )
-    $Uri = "https://admin.microsoft.com/fd/jarvisCM/my-org/profiles?type=organization"
+    #$Uri = "https://admin.microsoft.com/fd/jarvisCM/my-org/profiles?type=organization"
     $Uri = "https://admin.microsoft.com/fd/commerceMgmt/billingaccount"
     $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
     $Output
