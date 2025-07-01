3626e783e74cde24768d39c0f2eb27edc596c45f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 20:37:17 +0200

    not working

--- Examples/NotWorking/GetO365NotWorking.ps1
+++ Examples/NotWorking/GetO365NotWorking.ps1
@@ -7,12 +7,4 @@ if (-not $Credentials) {
 # keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
 $null = Connect-O365Admin -Verbose -Credential $Credentials
 
-# Not working
-Get-O365OrgNews -Verbose # problems
-Get-O365OrgScripts -Verbose # problems
-Get-O365OrgWhiteboard -Verbose # problem
-Get-O365OrgCommunicationToUsers -Verbose # no data
-Get-O365OrgMicrosoftSearch -Verbose # problems
-Get-O365SearchIntelligenceBingConfigurations # problems
-Get-O365BillingInvoices # not working
 Get-O365BillingProfile # not working
