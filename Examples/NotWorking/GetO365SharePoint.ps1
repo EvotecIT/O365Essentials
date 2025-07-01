059b331b36510f147e3204f528d7cb46f1322d4e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 20:37:42 +0200

    not working

---
+++ Examples/NotWorking/GetO365SharePoint.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module .\O365Essentials.psd1 -Force
+
+# This makes a connection to Office 365 tenant, using credentials
+# keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
+$Headers = Connect-O365Admin -Verbose
+
+
+$Uri = "https://compliance.microsoft.com/apiproxy/spo/admin/_api/SPO.Tenant/EnableAIPIntegration"
+$Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Verbose
+$Output
+
+$Uri = "https://compliance.microsoft.com/apiproxy/psws/PolicyConfig"
+$Output = Invoke-O365Admin -Uri $Uri -Headers $Headers -Verbose
+$Output
