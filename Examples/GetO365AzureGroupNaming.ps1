2799f2d6f21dab250525e13fce0908404cbd5a1d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 20:34:43 +0200

    Refactor example scripts to remove credential prompts for Office 365 connections, enhancing the connectivity process by allowing for MFA handling.

--- Examples/GetO365AzureGroupNaming.ps1
+++ Examples/GetO365AzureGroupNaming.ps1
@@ -1,12 +1,9 @@
 ﻿Import-Module .\O365Essentials.psd1 -Force
 
-if (-not $Credentials) {
-    $Credentials = Get-Credential
-}
 # This makes a connection to Office 365 tenant
 # since we don't want to save the data we null it out
 # keep in mind that if there's an MFA you would be better left without Credentials and just let it prompt you
-$null = Connect-O365Admin -Verbose -Credential $Credentials
+$null = Connect-O365Admin -Verbose
 
 Get-O365AzureGroupNamingPolicy -Verbose | Format-Table
 
