9c8bb866cb1e13d787728a2cbd32c7f8e8a0d99d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-01 20:35:08 +0200

    Refactor Get-O365BillingLicenseAutoClaim function to streamline code and improve readability by removing unnecessary comments and simplifying tenant ID retrieval.

--- Public/Get-O365BillingLicenseAutoClaim.ps1
+++ Public/Get-O365BillingLicenseAutoClaim.ps1
@@ -1,20 +1,4 @@
-﻿<# Not sure where I got this from
-function Get-O365BillingLicenseAutoClaim {
-    [cmdletbinding()]
-    param(
-        [alias('Authorization')][System.Collections.IDictionary] $Headers
-    )
-    if ($Headers) {
-        $TentantID = $Headers.Tenant
-    } else {
-        $TentantID = $Script:AuthorizationO365Cache.Tenant
-    }
-    $Uri = "https://admin.microsoft.com/fd/m365licensing/v1/tenants/$TentantID/licenseddevicesassets"
-    $Output = Invoke-O365Admin -Uri $Uri -Headers $Headers
-    $Output.items
-}
-#>
-function Get-O365BillingLicenseAutoClaim {
+﻿function Get-O365BillingLicenseAutoClaim {
     <#
     .SYNOPSIS
     Retrieves information about licensed devices assets for a specific Office 365 tenant.
