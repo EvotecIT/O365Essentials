0437d360da9ecfab2a6c3a197960aadb1c1a9f7b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-07-03 09:44:49 +0200

    feat(ConvertTo-Base64): ✨ Add ConvertTo-Base64Url function
    * Introduced a new function `ConvertTo-Base64Url` for encoding byte arrays to Base64 URL format.
    * This function replaces characters to ensure compatibility with URL encoding.

---
+++ Private/ConvertTo-Base64.ps1
@@ -0,0 +1,8 @@
+﻿function ConvertTo-Base64Url {
+    [cmdletbinding()]
+    param(
+        [byte[]] $bytes
+    )
+    [Convert]::ToBase64String($bytes).TrimEnd('=')
+    | ForEach-Object { $_.Replace('+', '-').Replace('/', '_') }
+}
